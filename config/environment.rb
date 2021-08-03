5e7bdae7d42811dece8e62727d33215fef7d0282
JoeMilius <joemilius@gmail.com>
2021-08-03 00:59:45 -0600

    Activities Post path established

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,13 @@
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= "development"
 
+
 # Require in Gems
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
+
+
 # Require in all files in 'app' directory
+
 require_all 'app'
