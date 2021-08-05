605051a93fb865cff3470fd82143ddaeea3ee0a3
JoeMilius <joemilius@gmail.com>
2021-08-05 10:05:14 -0600

    get query and response for all trips, days, activities

--- app/models/trips.rb
+++ app/models/trips.rb
@@ -1,4 +1,8 @@
 class Trip < ActiveRecord::Base
     belongs_to :traveler
     has_many :days, dependent: :destroy
+
+    def self.get_all
+        self.all.includes(:days).includes(:activities)
+    end
 end
