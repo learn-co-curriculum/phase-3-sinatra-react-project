e1858ed31620c92a0a1b50c8621453af6652e44d
TaraMKelly <tara.kelly16@gmail.com>
2021-08-23 18:04:50 -0400

    edited animal log table migration

--- db/migrate/20210823210734_create_animal_logs.rb
+++ db/migrate/20210823210734_create_animal_logs.rb
@@ -3,12 +3,12 @@ class CreateAnimalLogs < ActiveRecord::Migration[6.1]
     create_table :animal_logs do |t|
 
       t.string :note
+      t.datetime :time
+      t.boolean :pooped
+      t.boolean :fed
       t.belongs_to :zookeeper, foreign_key: true
       t.belongs_to :animal, foreign_key: true
-      t.boolean :pooped
       t.time_stamps
-      t.boolean :fed
-      t.datetime :last_fed
 
     end
   end
