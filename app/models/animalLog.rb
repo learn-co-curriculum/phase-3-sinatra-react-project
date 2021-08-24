6726f88e7a1d323234806a2d65fc2a4bfcf19fa5
Jessica J Lee <79885174+ellijs@users.noreply.github.com>
2021-08-24 04:10:35 -0400

    Update animalLog.rb

--- app/models/animalLog.rb
+++ app/models/animalLog.rb
@@ -1,5 +1,9 @@
 class AnimalLog < ActiveRecord::Base
     belongs_to :animal 
     belongs_to :zookeeper 
+    
+    def log_time=(time)
+        self.updated_at = AnimalLog.find_or_create_by(time: time)
+    end
 
 end
