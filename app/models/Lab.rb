fd1b0429468404b09d73b08538abfac38834f257
Michael <mkenton2020@icloud.com>
2021-09-13 17:44:54 -0500

    add student and lab models

---
+++ app/models/Lab.rb
@@ -0,0 +1,3 @@
+#Lab model code here. Lab belongs to student
+#Labs contain data such as num of commits, time of submission, lines of code (additions/changes)
+#Further data may include calcualted time taken to complete (date-time completed minus date-time forked)
