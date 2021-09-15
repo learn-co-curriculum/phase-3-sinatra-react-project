ac0f4f8db334674c287e43b87c604744be80c685
Sungoh Park <spark449062@gmail.com>
2021-09-15 13:58:06 -0600

    cards and characters seeded and attack tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/characters" do
-    characters = Character.all
+    Character.all.to_json
+  end
+
+  get "/cards" do
+    Card.all.to_json
+  end
+
+  get "/play_card/:id" do
+    card = Card.find(params[:id])
+    card.play(player, enemy)
     characters.to_json
   end
 
