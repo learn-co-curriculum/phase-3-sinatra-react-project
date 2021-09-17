da61d8313536af792d4149d8391ebb70e1fa80b1
Michael <mkenton2020@icloud.com>
2021-09-17 00:01:33 -0500

    updated schema with tasked

--- app/controllers/lab_controller.rb
+++ app/controllers/lab_controller.rb
@@ -9,14 +9,29 @@ class LabController < ApplicationController
         Lab.find(params[:id]).to_json
     end
 
+    get "/labs/:studentA/:studentB" do 
+        @labs = Lab.all
+        @studentA = Student.findBy(user_name: params[":studentA"])
+        @studentB = Student.findBy(user_name: params[":studentB"])
+        @labs = @labs.where(completed: "No", student_id: @studentA.id)
+    end
+
     get "/students/:student" do 
         Student.find(params[:student_id]).get_labs_by_student.to_json
     end
 
+    # patch "/labs/:id" do
+    #     lab = Lab.find(params[:id])
+    #     lab.update(
+    #         completed: params[:completed]
+    #     )
+    #     lab.to_json
+    # end
+
     patch "/labs/:id" do
         lab = Lab.find(params[:id])
         lab.update(
-            completed: params[:completed]
+            tasked: params[:tasked]
         )
         lab.to_json
     end
