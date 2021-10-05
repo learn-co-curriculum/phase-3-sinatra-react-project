86f6b767f677cea1832ae651c9e278f3199d2d4c
+ <+>
2021-10-05 15:46:32 -0700

    endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,60 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/students" do
-    allStudents = Student.all
-    allStudents.to_json
+
+  get "/tutors/:id/requests" do
+    Match.where(tutor_id: params[:id]).to_json
   end
 
-  get "/tutor" do
-    allStudents = Tutor.all
-    allStudents.to_json
+  get "/tutors/:id/profile" do
+    Tutor.find(params[:id]).to_json
   end
 
-  get "/match" do
-    allStudents = Match.all
-    allStudents.to_json
+  get "/tutors/:id/students" do
+    Match.where(tutor_id: params[:id]).where(matched: true).map(&:student).to_json
+  end
+
+  get "/students/tutors" do
+    Tutor.all.to_json
+  end
+
+  get "/students/:id/profile" do
+    Student.find(params[:id]).to_json
+  end
+
+  get "/students/:id/tutors" do
+    Match.where(student_id: params[:id]).where(matched:true).map(&:tutor).to_json
+  end
+
+  post "/tutor_sign_up" do
+    new_tutor = Tutor.create(
+    name: params[:name],
+    location: params[:location],
+    subjects: params[:subjects],
+    rate: params[:rate],
+    online_in_person: params[:online_in_person],
+    gender: params[:gender],
+    bio: params[:bio],
+    availability: params[:availability],
+    background_check: params[:background_check],
+    timezone: params[:timezone],
+    username: params[:username],
+    password: params[:password],
+    img: params[:img]
+    )
+    new_tutor.to_json
+  end
+
+  post "/student_sign_up" do
+    new_student = Student.create(
+      name: params[:name],
+      grade: params[:grade],
+      age: params[:age],
+      timezone: params[:timezone],
+      username: params[:username],
+      password: params[:password],
+      img: params[:img]
+    )
+    new_student.to_json 
   end
 
 end
