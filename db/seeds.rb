86c2041a8d21e848778546ff75b0e2e9f816a166
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-12 10:32:03 -0500

    Create Applications table and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+Application.destroy_all
+Application.reset_pk_sequence
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Application.create()
+Application.create()
+Application.create()
+Application.create()
+Application.create()
 
 puts "✅ Done seeding!"
