8cf4c328dfbe82eecf451d08ce43e778f70ba92f
Katelyn <katelynjewel@gmail.com>
2021-11-15 15:57:59 -0800

    seeding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,41 @@
 puts "🌱 Seeding spices..."
+puts "Deleteing previous data..."
+Animal.destroy_all
+Shelter.destroy_all
+AdoptionApplication.destroy_all
 
-# Seed your database here
+puts "🐩 Creating animals..."
+20.times do
+    Animal.create(
+        name: Faker::Creature::Dog.name, 
+        type: "dog", 
+        breed: Faker::Creature::Dog.breed, 
+        img_url: Faker::LoremFlickr.image(size: "150x160", search_terms: ['dog', 'cute'], match_all: true),
+        shelter_id: Shelter.all.sample.id
+    )
+end
+20.times do 
+    Animal.create(
+        name: Faker::Creature::Cat.name, 
+        type: "cat", 
+        breed: Faker::Creature::Cat.breed, 
+        img_url: Faker::LoremFlickr.image(size: "150x160", search_terms: ['cat', 'cute'], match_all: true),
+        shelter_id: Shelter.all.sample.id
+    )
+end
+
+puts "🏞️ Creating shelters..."
+40.times do 
+    Shelter.create(name: Faker::Educator.campus, address: Faker::Address.full_address)
+end
+
+puts "📝 Creating adoption applications..."
+40.times do 
+    AdoptionApplication.create(
+        name: Faker::Name.name, 
+        date: Faker::Date.in_date_period(month: 3), 
+        animal_id: Animal.all.sample.id  
+    )
+end
 
 puts "✅ Done seeding!"
