ea054bf9703081cfe605036d496fc591ee075393
Saim Rizvi <shr2mz@gmail.com>
2021-11-15 15:10:26 -0500

    added post/patch/delete for app and comm controllers

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,6 +7,27 @@ class ApplicationsController < ApplicationController
     Application.find(params[:id]).to_json
   end
 
+  post '/applications' do
+    application = Application.create(
+      company: params[:company],
+      position: params[:position],
+      status: params[:status],
+      logo_url: params[:logo_url]
+    )
+    application.to_json
+  end
+
+  patch '/applications/:id' do
+    application = Application.find(params[:id])
+    application.update(
+      company: params[:company],
+      position: params[:position],
+      status: params[:status],
+      logo_url: params[:logo_url]
+    )
+    application.to_json
+  end
+
   delete "/applications/:id" do
     Application.find(params[:id]).destroy.to_json
   end
