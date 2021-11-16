5275f842d593dc3292fbc3d309596610c818c744
Justin Chittarath <justinchitt@gmail.com>
2021-11-16 16:40:39 -0700

    endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,22 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/animals" do
+
     Animal.all.to_json
+  end
+
+  get "/animals/:id" do
+    animal = Animal.find(params[:id])
+    animal.name.to_json
   end
 
   get "/shelters" do
     Shelter.all.to_json
   end
 
-  get "shelters/:id" do
-    binding.pry
+  get "/shelters/:id" do
     shelter = Shelter.find(params[:id])
-    shelter.to_json
+    shelter.name.to_json
   end
 
   get "/applications" do
