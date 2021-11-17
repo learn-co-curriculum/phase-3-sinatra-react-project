38427e13d0894436c4c6ceaeb535c28a0b4404a9
Justin Chittarath <justinchitt@gmail.com>
2021-11-16 17:46:21 -0700

    oops pushin to main

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,28 @@ class ApplicationController < Sinatra::Base
     AdoptionApplication.all.to_json
   end
 
+  post "/animals" do
+    shelter = Shelter.find_by_name(params[:shelter]).id
+    animal = Animal.create(
+      name: params[:name],
+      animal_type: params[:animal_type],
+      breed: params[:breed],
+      img_url: params[:img_url],
+      shelter_id: shelter
+    )
+    animal.to_json
+  end
+
+  patch "/animals/:id" do
+    shelter = Shelter.find_by_name(params[:shelter]).id
+    animal = Animal.find(params[:id]).update(
+      name: params[:name],
+      animal_type: params[:animal_type],
+      breed: params[:breed],
+      img_url: params[:img_url],
+      shelter_id: shelter
+    )
+    animal.to_json
+  end
+
 end
