4f9f1aebf5ccbb00cc040d9231daa451ba92539e
Katelyn <katelynjewel@gmail.com>
2021-11-17 08:43:25 -0800

    post and patch done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/animals" do
-
     Animal.all.to_json
   end
 
@@ -46,6 +45,42 @@ class ApplicationController < Sinatra::Base
       shelter_id: shelter
     )
     animal.to_json
+  end
+
+  post "/applications" do
+    animal = Animal.find_by_name(params[:animal]).id
+    application = AdoptionApplication.create(
+      name: params[:name],
+      date: params[:date],
+      animal_id: animal
+    )
+    application.to_json
+  end
+
+  patch "/applications/:id" do
+    animal = Animal.find_by_name(params[:animal]).id
+    application = AdoptionApplication.create(
+      name: params[:name],
+      date: params[:date],
+      animal_id: animal
+    )
+    application.to_json
+  end
+
+  post "/shelters" do
+    Shelter.create(
+      name: params[:name],
+      address: params[:address],
+    )
+    Shelter.to_json
+  end
+
+  patch "/shelters" do
+    Shelter.create(
+      name: params[:name],
+      address: params[:address],
+    )
+    Shelter.to_json
   end
 
 end
