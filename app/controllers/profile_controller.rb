91fe4ac93ce4d5b4fbe7f06cff78939c34e53b4e
garrettdick <garrettd0511@gmail.com>
2021-11-18 10:13:36 -0600

    Created post and delete routes

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,6 +5,19 @@ class ProfileController < ApplicationController
     get "/profiles" do
       Profile.all.to_json
     end
+
+    post "/profiles" do # This should be able to create new users 
+      new_profile = Profile.create(
+        user_name: params[:user_name]
+        profile_img: params[:profile_img]
+      )
+      new_profile.to_json
+    end
+
+    delete "/profiles/:id" do # This should be able to delete users 
+      delete_user = Profile.find(params[:id])
+      delete_user.destroy
+    end
   
   end
   
