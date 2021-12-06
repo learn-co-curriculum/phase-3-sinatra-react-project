ff3b35e52784e12235d9cb9308a38b7c7c430c2b
Daniel Hyman <dahyman91@gmail.com>
2021-12-06 12:13:20 -0500

    created endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,24 @@ class ApplicationController < Sinatra::Base
     post = Post.all.order(:created_at)
     post.to_json
   end
+
+  get '/categories' do
+    category = Category.all
+    category.to_json
+  end
+
+  get '/category/id=:id' do
+    category = Category.find(params[:id])
+    category.to_json
+  end
+
+  get '/category/name=:name' do
+    category = Category.find_by(name: params[:name])
+    category.to_json
+  end
+
+  get '/category_posts/name=:name' do
+    category = Category.find_by(name: params[:name]).posts
+    category.to_json
+  end
 end
