4b8e05b65b2017a30dc9ce4c6c6c2f66ba0af084
Jeremiah <jeremiahmarcos27@outlook.com>
2021-12-08 08:54:27 -0600

    cleaned up a few lines of code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ActiveRecord::Base
     has_many :reviews
-    has_many :watch_lists
     has_many :movies, through: :reviews
     has_many :shows, through: :reviews
-    has_many :movies, through: :watch_lists
-    has_many :shows, through: :watch_lists
+
     
 
 
