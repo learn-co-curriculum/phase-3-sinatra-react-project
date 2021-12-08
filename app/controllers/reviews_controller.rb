df8679f1acc325fa8bcbc3207c658539dc23a62c
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-12-07 20:25:00 -0500

    pulled from main branch and added Delete route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,24 @@ class ReviewsController < ApplicationController
     reviews = Review.all
     reviews.to_json
   end
+
+  delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    {message:'review destroyed'}.to_json
+  end
+
 end
+
+#handle delete for front end
+# const handleDelete = (id) => {
+#   fetch(`http://localhost:9292/reviews/${id}`, {
+#     method: 'DELETE',
+#     headers: {
+#       'Content-Type': 'application/json'
+#     }
+#   })
+#   .then(res => res.json())
+#   .then(() =>
+#   setReviews(reviews.filter(r => r.id !== id)))
+# }
