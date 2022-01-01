3e9f7e3c0fa3d62a0003af0855e8b7edd7a077a0
Lauren Hess <laurenbethhess@gmail.com>
2022-01-01 17:09:29 -0500

    added patch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,4 +25,10 @@ class ApplicationController < Sinatra::Base
     todo.to_json
   end
 
+  patch "/todos/:id" do
+    todo = Todo.find(params[:id])
+    todo.update(item: params[:item])
+    todo.to_json(include: { category: {only: [:category]} })
+  end
+
 end
