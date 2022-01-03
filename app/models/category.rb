7c286d289b13d1e11cc98b65523b578dd2e07b8f
Lauren Hess <laurenbethhess@gmail.com>
2022-01-03 17:11:05 -0500

    created branch

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,14 @@
 class Category < ActiveRecord::Base
     has_many :todos
 
+    def self.create_new(id, category)
+        self.create(id: id, category: category)
+    end
+
+    def delete(id)
+        self.find(id).destroy
+    end
+
+
+
 end
--- app/models/todos.rb
+++ app/models/todos.rb
@@ -4,4 +4,14 @@ class Todo < ActiveRecord::Base
     def self.all_items
         self.all.order(item: :asc).pluck(:item)
     end
+
+    def self.all_importance
+        self.all.order(importance: :asc)
+    end
+
+    def self.filter_importance(importance)
+        self.all.filter{|todo| todo[:importance] === importance}
+    end
+
+
 end
