b02adf5664ff12261b7cd7151ded09a86a25697d
Jake <jakerfilut@gmail.com>
2022-01-04 16:00:25 -0700

    test

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -15,5 +15,22 @@ class ProductController < ApplicationController
         )
       end  
 
+      get '/products/:id' do
+        Product.find(params[:id]).to_json
+    end
+
+    patch '/products/:id' do
+      product = Product.find(params[:id])
+      product.update(
+      {name: params[:name],
+      department: params[:department],
+      description: params[:description],
+      unit_price: params[:unit_price],
+      img_url: params[:img_url],
+      inventory: params[:inventory]}
+      )
+      product.to_json
+    end
+
 
 end
