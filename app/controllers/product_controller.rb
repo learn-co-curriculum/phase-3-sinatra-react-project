0a225429012865187ef0d53e71ea2da486ec9787
Ben <benbperry1@gmail.com>
2022-01-04 11:32:47 -0800

    setting up product routes

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,18 +2,39 @@ class ProductController < ApplicationController
 
     get '/products' do
         Product.all.to_json
-      end
+    end
+
+    get '/products/:id' do
+        Product.find(params[:id]).to_json
+    end
 
     post '/products' do
         product = Product.create(
         {name: params[:name],
-        category: params[:category],
+        department: params[:department],
         description: params[:description],
         unit_price: params[:unit_price],
         img_url: params[:img_url],
         review: params[:review]}
         )
-      end  
+    end
+
+    patch '/products/:id' do
+      product = Product.find(params[:id])
+      product.update(
+      {name: params[:name],
+      department: params[:department],
+      description: params[:description],
+      unit_price: params[:unit_price],
+      img_url: params[:img_url],
+      clinic_id: params[:clinic_id],
+      review: params[:review]}
+      )
+    end  
+    
+
 
 
 end
+
+
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
 has_many :order_items
 has_many :orders, through: :order_items
+has_many :reviews
 
 
 
