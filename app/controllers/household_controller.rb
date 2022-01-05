a095e13a3b3095d79027e6a166d77fb8a0c601de
smh1988-2 <seamhurley1988@gmail.com>
2022-01-04 21:38:49 -0800

    Added comments

--- app/controllers/household_controller.rb
+++ app/controllers/household_controller.rb
@@ -5,6 +5,7 @@ class HouseholdController < Sinatra::Base
       Household.all.to_json(include: :users)
     end
 
+  # Gets all the task data associated with a household via tasks (who the task is assigned to and which pet the task is for)
     get "/households/:id/all" do
       Household.find(params[:id]).to_json(include: {
         tasks: {
@@ -21,11 +22,13 @@ class HouseholdController < Sinatra::Base
       Household.find(params[:id]).to_json(include: :users)
     end
 
+    # Gets all the pet data associated with a household
     get "/households/:id/pets" do
       Household.find(params[:id]).to_json(include: :pets)
     end
 
-    get "/households/:id/tasks" do
+  # Gets all the task data associated with a household via tasks (who the task is assigned to and which pet the task is for)
+  get "/households/:id/tasks" do
       Household.find(params[:id]).to_json(include: {
         tasks: {
           include: [
