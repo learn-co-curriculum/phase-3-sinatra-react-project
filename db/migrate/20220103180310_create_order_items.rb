4e78173424cc3cab12db099830d763691872d67d
Ben <benbperry1@gmail.com>
2022-01-04 17:47:10 -0800

    subtotal added

--- db/migrate/20220103180310_create_order_items.rb
+++ db/migrate/20220103180310_create_order_items.rb
@@ -3,7 +3,6 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
     create_table :order_items do |t|
       t.integer :quantity
       t.decimal :unit_price
-      # t.decimal :total
       t.references :order
       t.references :product
       t.timestamps
