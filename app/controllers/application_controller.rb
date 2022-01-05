9284dc0e2a82d4f88c3bebba41354f47e414db29
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-01-04 19:24:30 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/products" do
+    { message: "Get products!" }.to_json
+  end
+
+  get "/products/:id" do 
+    { message: "See details on one product" }.to_json
+  end
+
+  delete "/products/:id" do
+    { message: "Delete a product after it has been sold" }.to_json
+  end
+
+  get "/orders" do
+    { message: "See items in your shopping cart" }.to_json
+  end
+
+
 end
