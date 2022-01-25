a6dfe2b017a4aeba08d3e6095767b8ac28f47cdb
tyler <tymaggio03@gmail.com>
2022-01-25 16:43:39 -0700

    start of ruby backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+require 'faker'
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+
+ 100.times do 
+    Company.create(
+        company_name: Faker::Company.name,
+    )
+end
 
 puts "✅ Done seeding!"
