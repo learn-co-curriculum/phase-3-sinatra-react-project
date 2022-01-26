c89959f8bf47a8cdc886bef75f7d786ff13ef77f
Ahmet Uysaler <ahmetsenih@gmail.com>
2022-01-26 10:40:39 -0600

    fixed post error

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -13,7 +13,7 @@ class ParksController < ApplicationController
     post '/parks' do
         park = Park.create(park_name: params[:park_name], description: params[:description], fee: params[:fee], image_url: params[:image_url],
             location: params[:location], states: params[:states], website: params[:website], state_id: params[:state_id], list_id: params[:list_id])
-        park.to_json({include: :list, :state}
+        park.to_json({include: :list, include: :state})
     end
 
 
