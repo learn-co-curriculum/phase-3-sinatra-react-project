cea5ca4caf31a215472f504ab761f829b6b2be03
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2022-01-25 21:31:50 -0500

    debugging

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -10,11 +10,11 @@ class ParksController < ApplicationController
     end
 
     # needs to be checked
-    post '/parks' do
-        park = Park.create(park_name: params[:park_name], description: params[:description], fee: params[:fee], image_url: params[:image_url],
-            location: params[:location], states: params[:states], website: params[:website], state_id: params[:state_id], list_id: params[:list_id])
-        park.to_json({include: :list, :state}
-    end
+    # post '/parks' do
+    #     park = Park.create(park_name: params[:park_name], description: params[:description], fee: params[:fee], image_url: params[:image_url],
+    #         location: params[:location], states: params[:states], website: params[:website], state_id: params[:state_id], list_id: params[:list_id])
+    #     park.to_json({include: :list, :state}
+    # end
 
 
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
     has_many :parks
     has_many :states, through: :parks
+
+    
 end
