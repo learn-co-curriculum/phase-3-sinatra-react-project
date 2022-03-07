c5ef86ef9374538691cc627b82288b87d1398041
Felipa Mendez <felipalouise@gmail.com>
2022-03-07 12:02:39 -0600

    added migrations

---
+++ db/migrate/20220307175647_create_posts.rb
@@ -0,0 +1,7 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :content
+    end
+  end
+end
---
+++ db/migrate/20220307175851_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.integer :post_id
+      t.integer :user_id
+    end
+  end
+end
