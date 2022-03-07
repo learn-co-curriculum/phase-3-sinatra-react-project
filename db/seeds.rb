b649613cc15fe4dbba9ce6beaccf275f72bf54dd
Matt Draghi <mtd5264@gmail.com>
2022-03-07 18:06:21 -0500

    Set up seed data template

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-#Artists
-a1 = Artist.create(name: "", followers: , about: "")
-a1 = Artist.create(name: "", followers: , about: "")
-a1 = Artist.create(name: "", followers: , about: "")
-
 #Songs
 s1 = Song.create(title: "", album_id: , artist_id: , genre: "", youtube_link: "", duration: )
 s1 = Song.create(title: "", album_id: , artist_id: , genre: "", youtube_link: "", duration: )
@@ -15,15 +10,13 @@ s1 = Song.create(title: "", album_id: , artist_id: , genre: "", youtube_link: ""
 s1 = Song.create(title: "", album_id: , artist_id: , genre: "", youtube_link: "", duration: )
 
 #Albums
-alb1 = Album.create(title: "", release_date: "", downloads: "", duration: "")
-alb1 = Album.create(title: "", release_date: "", downloads: "", duration: "")
-alb1 = Album.create(title: "", release_date: "", downloads: "", duration: "")
-alb1 = Album.create(title: "", release_date: "", downloads: "", duration: "")
+Album.create(title: "", release_date: "", downloads: , duration: )
 
+#Artists
+Artist.create(name: "", followers:  , about: "")
 
 #Playlist
-p1 = Playlist.create(user_id: u1.id , creation_date: Date.new , last_update: Date.new, songs: "", duration: )
-
+# p1 = Playlist.create(user_id: u1.id , creation_date: Date.new , last_update: Date.new, duration: )
 
 #User
 u1 = User.create(user_name: "test_user", password: "test123", email: "test_user@test.com")
