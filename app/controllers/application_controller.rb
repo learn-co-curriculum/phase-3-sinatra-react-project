e23c1e595ec1c1e004b1f787303fd606c8f48d63
Felipa Mendez <felipalouise@gmail.com>
2022-03-07 16:48:45 -0600

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  # get "/users" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
+
+
 end
