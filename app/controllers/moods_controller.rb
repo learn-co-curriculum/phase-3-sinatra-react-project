b77f5ad1f02da4de8c036b2a5df813f8057bf666
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-03-07 17:20:28 -0600

    get activities for mood 1 worked

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,10 +1,10 @@
 class MoodsController < ApplicationController
 
     get "/chill" do
-        binding.pry
+        
 
-        # activities = Activity.all.where('mood_id: 1')
-        # activities.to_json()
+        activities = Activity.all.where(mood_id: 1)
+        activities.to_json()
         
     end
 
