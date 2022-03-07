6bde4a54bf821c26074611f47ea6329aa113df2c
Scarlett Eller <rq6715@gmail.com>
2022-03-07 16:48:05 -0500

    added initial controller commands

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/profiles" do
+    profiles = Profile.all 
+    profiles.to_json
+  end
+
+  get "/likes" do
+    likes = Like.all
+    likes.to_json
+  end
+
+  get "/users" do 
+    users = User.all
+    users.to_json
   end
 
 end
