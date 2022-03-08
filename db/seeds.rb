c469d42cbb3cb5b5d8e2e2c4a8a24a0fbb3f20e2
Jon-Collin Chinje <j.chinje@gmail.com>
2022-03-08 10:24:25 -0500

    added album image links to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ beatles = Artist.create(name: "The Beatles", followers: 202131  , about: "An Eng
 steve =  Artist.create(name: "Steve Lacy", followers: 142131  , about: "An American musician, singer, songwriter, and record producer. He was born in Compton, California, and gained recognition as the guitarist of the R&B group the Internet in 2015.") 
 
 #Albums
-okorc = Album.create(title: "OK ORCHESTRA", artist_id: ajr.id, release_date: "2021", downloads: 1002992, duration: 2748 )
-mtjoyalb = Album.create(title: "Mt. Joy", artist_id: mtjoy.id, release_date: "2018", downloads: 22343, duration: 2669)
-humancon = Album.create(title: "The Human Condition", artist_id: jonbel.id, release_date: "2016", downloads: 945322, duration: 3317)
-guns = Album.create(title: "Go For Your Guns", artist_id: isley.id, release_date: "1977", downloads: 25354, duration: 2005)
-strawberry = Album.create(title: "Strawberry Fields Forever / Penny Lane", artist_id: beatles.id , release_date: "1967", downloads: 2532154, duration: 449)
-demo = Album.create(title: "Steve Lacy Demo", artist_id: steve.id , release_date: "2017", downloads: 233254, duration: 813)
+okorc = Album.create(title: "OK ORCHESTRA", artist_id: ajr.id, release_date: "2021", downloads: 1002992, duration: 2748, album_cover: "https://upload.wikimedia.org/wikipedia/en/b/bf/AJR_-_OK_Orchestra.png")
+mtjoyalb = Album.create(title: "Mt. Joy", artist_id: mtjoy.id, release_date: "2018", downloads: 22343, duration: 2669, album_cover: "https://yt3.ggpht.com/ytc/AKedOLTvN2FV2a9WxJQxaPRgtUd6cvQ8secQiHSNzVmWOA=s900-c-k-c0x00ffffff-no-rj")
+humancon = Album.create(title: "The Human Condition", artist_id: jonbel.id, release_date: "2016", downloads: 945322, duration: 3317, album_cover: "https://upload.wikimedia.org/wikipedia/en/4/4d/The_Human_Condition_%28Official_Album_Cover%29_by_Jon_Bellion.png")
+guns = Album.create(title: "Go For Your Guns", artist_id: isley.id, release_date: "1977", downloads: 25354, duration: 2005, album_cover: "https://m.media-amazon.com/images/I/51W5GRn6gPL._SY355_.jpg")
+strawberry = Album.create(title: "Strawberry Fields Forever / Penny Lane", artist_id: beatles.id , release_date: "1967", downloads: 2532154, duration: 449, album_cover: "https://upload.wikimedia.org/wikipedia/en/6/69/Pennystrawps.jpg")
+demo = Album.create(title: "Steve Lacy Demo", artist_id: steve.id , release_date: "2017", downloads: 233254, duration: 813, album_cover: "https://direct.rhapsody.com/imageserver/images/alb.253069031/500x500.jpg")
 
 #Songs
 s_footsteps = Song.create(title: "Footsteps in the Dark", album_id: guns.id, genre: "soul", youtube_link: "https://www.youtube.com/watch?v=Dyq9zlYMw9g&ab_channel=TheIsleyBrothers-Topic", duration: 306)
