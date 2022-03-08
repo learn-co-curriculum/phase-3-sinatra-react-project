2640e93a44f3937353c534335894356095cf6c7c
Jon-Collin Chinje <j.chinje@gmail.com>
2022-03-08 09:25:08 -0500

    added durations to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 #Songs
-s_footsteps = Song.create(title: "Footsteps in the Dark", album_id: guns.id , artist_id: isley.id, genre: "soul", youtube_link: "https://www.youtube.com/watch?v=Dyq9zlYMw9g&ab_channel=TheIsleyBrothers-Topic", duration: )
+s_footsteps = Song.create(title: "Footsteps in the Dark", album_id: guns.id , artist_id: isley.id, genre: "soul", youtube_link: "https://www.youtube.com/watch?v=Dyq9zlYMw9g&ab_channel=TheIsleyBrothers-Topic", duration: 306)
 s_bang = Song.create(title: "Bang!", album_id: okorc.id, artist_id: ajr.id, genre: "indie", youtube_link: "https://www.youtube.com/watch?v=4THFRpw68oQ", duration:170 )
 s_sheep = Song.create(title: "Sheep", album_id: mtjoyalb.id, artist_id: mtjoy.id, genre: "indie", youtube_link: "https://www.youtube.com/watch?v=Xl1psdL6z0c", duration: 223)
 s_atl = Song.create(title: "All Time Low", album_id: humancon.id, artist_id: jonbel.id, genre: "pop", youtube_link: "https://www.youtube.com/watch?v=AXnqkVTFUqY", duration: 218)
 s_outro = Song.create(title: "Hand of God - Outro", album_id: humancon.id, artist_id: jonbel.id, genre: "pop", youtube_link: "https://www.youtube.com/watch?v=UYdmI0o3XNQ", duration: 338)
-s_strawberry = Song.create(title: "Strawberry Fields Forever", album_id: strawberry.id , artist_id: beatles.id , genre: "rock", youtube_link: "https://www.youtube.com/watch?v=HtUH9z_Oey8&ab_channel=TheBeatlesVEVO", duration: )
-s_red = Song.create(title: "Dark Red", album_id: red.id , artist_id: steve.id , genre: "indie", youtube_link: "https://www.youtube.com/watch?v=L0wusEVceek&ab_channel=SteveLacy-Topic", duration: )
+s_strawberry = Song.create(title: "Strawberry Fields Forever", album_id: strawberry.id , artist_id: beatles.id , genre: "rock", youtube_link: "https://www.youtube.com/watch?v=HtUH9z_Oey8&ab_channel=TheBeatlesVEVO", duration: 263)
+s_red = Song.create(title: "Dark Red", album_id: red.id , artist_id: steve.id , genre: "indie", youtube_link: "https://www.youtube.com/watch?v=L0wusEVceek&ab_channel=SteveLacy-Topic", duration: 173)
 #Albums
 okorc = Album.create(title: "OK ORCHESTRA", release_date: "2021", downloads: 1002992, duration: 2748 )
 mtjoyalb = Album.create(title: "Mt. Joy", release_date: "2018", downloads: 22343, duration: 2669)
 humancon = Album.create(title: "The Human Condition", release_date: "2016", downloads: 945322, duration: 3317)
-guns = Album.create(title: "Go For Your Guns", release_date: "1977", downloads: 25354, duration: )
-strawberry = Album.create(title: "Strawberry Fields Forever / Penny Lane", release_date: "1967", downloads: 2532154, duration: )
-demo = Album.create(title: "Steve Lacy Demo", release_date: "2017", downloads: 233254, duration: )
+guns = Album.create(title: "Go For Your Guns", release_date: "1977", downloads: 25354, duration: 2005)
+strawberry = Album.create(title: "Strawberry Fields Forever / Penny Lane", release_date: "1967", downloads: 2532154, duration: 449)
+demo = Album.create(title: "Steve Lacy Demo", release_date: "2017", downloads: 233254, duration: 813)
 
 #Artists
 ajr = Artist.create(name: "AJR", followers: 2136144 , about: "Re-envisioning what pop can be in the 21st century, AJR unassumingly emerged as a ubiquitous hit-making outlier and one of the biggest indie bands in the world.")
