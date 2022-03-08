b0241b688a1835ed64ac90e85a4564ee95407439
Kenneth Kebaara <kkebaara@yahoo.com>
2022-03-08 02:10:51 -0700

    added Faker gem and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
+GroceryList.destroy_all
+Category.destroy_all
+Item.destroy_all
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+
+20.times do
+    GroceryList.create(title: Faker::Name.name, category: Category.all.sample, item: Item.all.sample)
+end
+
+10.times do
+    Category.create(title: Faker::Name.name)
+end
+
+
+10.times do
+    Item.create(title: Faker::Name.name, quantity:rand(1..5))
+end
 
 
 
