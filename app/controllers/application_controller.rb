997aadfc9c3d7dcc1715c612dfac5df8337674de
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-03-08 10:22:42 -0500

    first get request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/buildings" do
+    buildings = Building.all
+    buildings.to_json
   end
 
 end
