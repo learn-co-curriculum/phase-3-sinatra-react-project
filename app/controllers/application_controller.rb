3c2123d00ac5577b576f0b2108ba86ecf314be5f
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-03-08 14:43:02 -0500

    initial rendering on front end, updated get methods to include city and architect obj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
   get "/buildings" do
     buildings = Building.all
-    buildings.to_json
+    # buildings.to_json
+    buildings.to_json(include: [:architect, :city])
   end
+
+  
 
   get '/buildings/:id' do
     building = Building.find(params[:id])
@@ -19,7 +22,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/buildings' do
-    building = Building.create(
+    new_building = Building.create(
       name: params[:name],
       city_id: params[:city_id],
       architect_id: params[:architect_id],
@@ -28,6 +31,6 @@ class ApplicationController < Sinatra::Base
       description: params[:description],
       map_location: params[:map_location]
     )
-    building.to_json
+    new_building.to_json
   end
 end
