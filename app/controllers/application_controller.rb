91c9db40a033ade7acd4ed8acd624d025fdb9e7d
Mike Starr <imhs@protonmail.com>
2022-03-07 16:13:35 -0800

    Basic frameworkto share with team members

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/read_expense/:id" do
+    expense = Expense.find(params[:id])
+    expense.to_json
+  end
+
+  get "/display_all_expenses" do
+    expense = Expense.all
+    expense.to_json
+  end
+
   post "/add_expense" do
     # binding.pry
     expense = Expense.create(
@@ -20,7 +30,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch "/update_expense/:id" do
-    # binding.pry
+    binding.pry
     expense = Expense.find(params[:id]).update(
       description: params[:description],
       monthly: params[:monthly],
@@ -38,6 +48,16 @@ class ApplicationController < Sinatra::Base
     expense.destroy
   end
 
+  get "/read_income/:id" do
+    income = Income.find(params[:id])
+    income.to_json
+  end
+
+  get "/read_all_income" do
+    income = Income.find.all
+    income.to_json
+  end
+
   post "/add_income" do
     # binding.pry
     income = Income.create(
@@ -48,7 +68,16 @@ class ApplicationController < Sinatra::Base
   end
 
   patch "/update_income/:id" do
+    income = Income.find(params[:id]).update(
+      description: params[:description],
+      amount: params[:amount]
+    )
+    income.to_json
+  end
+
+  delete '/delete_income/:id' do
     income = Income.find(params[:id])
+    income.destroy
   end
 
 end
