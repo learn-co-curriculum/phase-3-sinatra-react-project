0bc260d566e9317f0fb5739799751d3960e2176a
Matt Draghi <mtd5264@gmail.com>
2022-03-08 10:12:42 -0500

    Added get to song api

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -2,8 +2,17 @@ class SongController < Sinatra::Base
     set :default_content_type, 'application/json'
     
     # Add your routes here
-    get "/songs" do
 
+    #Read (all)
+    get "/songs" do
+        songs = Song.all
+        songs.to_json
     end
+
+    #Read (one)
+    get '/songs/:id' do
+        Song.find(params[:id]).to_json
+    end
+    
   
   end
