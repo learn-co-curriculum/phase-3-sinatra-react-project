62947d0776c8f1ba526ee1a2afab52aca0a696e1
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-03-08 10:42:21 -0600

    added sin routes for rest of moods

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,11 +1,30 @@
 class MoodsController < ApplicationController
 
     get "/chill" do
-        
-
         activities = Activity.all.where(mood_id: 1)
         activities.to_json()
-        
     end
+
+    get "/adventurous" do
+        activities = Activity.all.where(mood_id: 2)
+        activities.to_json()
+    end
+
+    get "/hungry" do
+        activities = Activity.all.where(mood_id: 3)
+        activities.to_json()
+    end
+
+    get "/creative" do
+        activities = Activity.all.where(mood_id: 4)
+        activities.to_json()
+    end
+
+    get "/hype" do
+        activities = Activity.all.where(mood_id: 5)
+        activities.to_json()
+    end
+
+
 
 end
