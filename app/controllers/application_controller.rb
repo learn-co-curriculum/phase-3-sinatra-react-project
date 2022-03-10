0fc0707086d7317a33eefc585ab0a16a26541d3f
Harris Sultan <harris_sultan@outlook.com>
2022-03-10 15:19:56 -0500

    modified sinatra stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,19 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/dogs" do
+    dogs=Dog.all
+    dogs.to_json  
+  end
+  get "/dogs/:id" do
+    dogs=Dog.find(params[:id])
+    dogs.to_json
+  end
+  delete "/dogs/:id" do
+    dogs=Dog.find(params[:id])
+    dogs.destroy
+    dogs.to_json
+  end
+
 
 end
