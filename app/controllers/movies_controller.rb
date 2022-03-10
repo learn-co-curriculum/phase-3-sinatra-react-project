6a927cbce49543f200c8263893d15996460f1407
Hamzah Contreras <Hamzah_36@outlook.com>
2022-03-10 09:07:17 -0600

    Changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
     get '/movies/:id' do
         movie = Movie.find(params[:id])
         movie.to_json(
-            include: { actors: { only: [:actor_name] } }
+            include: { actors: { only: [:name, :profile_image_url] }, reviews: { only: [:content]} }
         )
     end
 
