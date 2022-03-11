c94ff96036c4cf1d23fe6c80fa028932ce0126aa
karsyn ansari <karsyn.ansari@gmail.com>
2022-03-11 09:28:51 -0700

    post working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,10 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    # { message: "Good luck with your project!" }.to_json
-  end
-
-#   API Endpoints: 
-
-# GET: /poses
-# GET: /classes
-# POST: /classes
-# DELETE: /classes:id 
-
 get "/poses" do
   poses = Pose.all
   poses.to_json
-  # "hello World"
+ 
 end
 get "/yoga_classes" do
   yoga_classes = Yoga_Class.all
@@ -27,12 +15,13 @@ delete '/yoga_class/:id' do
   yoga_class.destroy
   yoga_class.to_json
 end
-post "/yoga_class" do
+post "/yoga_classes" do
   yoga_classes = Yoga_Class.create(
   teacher_name: params[:teacher_name],
   class_name: params[:class_name],
   class_time: params[:class_time]
   )
+  # call method here
   yoga_classes.to_json
   end
 
