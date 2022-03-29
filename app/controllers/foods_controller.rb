7e89bc15bb8dae00ff23ae3497961738e48dd851
Justin Swift <jlswift2@gmail.com>
2022-03-29 10:49:43 -0500

    Create Foods controller

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,13 @@
+class FoodsController < ApplicationController
+    
+    get '/foods' do
+      foods = Food.all
+      foods.to_json
+    end
+  
+    get "/foods/:id" do 
+      food = Food.find(params[:id])
+      food.to_json
+    end
+    
+end
