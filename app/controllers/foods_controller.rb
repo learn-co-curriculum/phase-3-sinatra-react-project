8119c614d0ab560c5da3e30ed51ad0a4d7537181
Michael Dally <mddally@gmail.com>
2022-03-30 13:58:38 -0400

    post method on foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,9 +1,18 @@
 class FoodsController < ApplicationController
 
-get '/foods' do 
-    foods = Food.all
-    foods.to_json
-end
+    get '/foods' do 
+        foods = Food.all
+        foods.to_json
+    end
 
+    post '/foods' do
+        newFood = Food.create({
+            name: params[:name],
+            quantity: params[:quantity],
+            user_id: params[:user_id],
+            fridge_id: params[:fridge_id]
+        })
+        newFood.to_json
+    end
 
 end
