8c45aa89895b740a5aa59440d4d24caf91de3e9e
Michelle Caraballo <caraballomp@yahoo.com>
2022-04-18 11:48:22 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_18_152905) do
-
-  create_table "creatures", force: :cascade do |t|
-    t.string "creature_type"
-    t.string "film"
-    t.string "image_url"
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
