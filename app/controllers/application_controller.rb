bf7db17221458c5137d6c38f40eed2f385084570
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-18 12:15:35 -0400

    added new routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/games" do
+    Game.all.order(:title).to_json
+  end
+
+  delete '/users/:id' do
+    user = User.find(params[:id])
+    user.destroy
+    user.to_json
+  end
+
+  #stand in for when we make game relationships editable
+  # patch '' do
+  #   game_relationship = GameRelationship.find(params[:id])
+  #   game_relationship.update()
+  #   game_relatinship.to_json
+  # end
+
 end
