8229ce4da3987187c83c5b9b9937e45ac270ec83
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-20 16:20:27 -0700

    init1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   before do
-    if token = request.env["HTTP_TOKEN"]
+    if token = request.env['HTTP_TOKEN']
       @user = User.find_by(token: token)
     end
-
   end
 
   def check_login
     return true if @user
-      
   end
-
 end
-
--- app/controllers/creature_controller.rb
+++ app/controllers/creature_controller.rb
@@ -1,30 +1,26 @@
 class CreatureController < ApplicationController
-
-
-    get "/creature" do
-        if check_login
-            Creature.all.to_json
-        else
-            return {login_error: false}.to_json
-        end
+  get '/creature' do
+    if check_login
+      Creature.all.to_json
+    else
+      return { login_error: false }.to_json
     end
+  end
 
-    patch "/creature/:id" do
-        if check_login
-            creature = Creature.find_by_id(params[:id])
-            creature.update(creature_params)
-            creature.to_json
-        else 
-            return {login_error: false}.to_json
-        end
+  patch '/creature/:id' do
+    if check_login
+      creature = Creature.find_by_id(params[:id])
+      creature.update(creature_params)
+      creature.to_json
+    else
+      return { login_error: false }.to_json
     end
+  end
 
-    private
+  private
 
-    def creature_params
-        allowed_params = %w(creatue_type film image_url)
-        params.filter do |param, value|
-          allowed_params.include?(params)
-        end
-    end
+  def creature_params
+    allowed_params = %w[creatue_type film image_url]
+    params.filter { |param, value| allowed_params.include?(params) }
+  end
 end
