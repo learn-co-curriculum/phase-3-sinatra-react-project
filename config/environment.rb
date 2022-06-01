95ea235f0c0decf63d246d2ec7fdd396cae1545f
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-01 16:59:23 -0400

    Code from Z's walkthrough

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,12 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :database => "db/#{ENV['RACK_ENV']}.sqlite"
+
+)
+
 # Require in all files in 'app' directory
+require './app/controllers/application_controller'
 require_all 'app'
---
+++ db/migrate/20220601205638_Code.rb
@@ -0,0 +1,4 @@
+class Code < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
