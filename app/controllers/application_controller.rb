673c85203fc87f30ccaff3908960d0ce78096125
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-01 15:34:51 -0400

    Testing github changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+  # this is a test for kate montgomery
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
