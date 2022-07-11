92cbd493e894253fc8401b4b715837966bd851fc
James Macapagal <j.macapagal@gmail.com>
2022-07-11 11:26:07 -0500

    stood up DB

--- app/models/habitat.rb
+++ app/models/habitat.rb
@@ -1,2 +1,4 @@
 class Habitat < ActiveRecord::Base
+    has_many :sightings
+    has_many :animals, through: :sightings
 end
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,2 +1,4 @@
 class Sighting < ActiveRecord::Base
+    belongs_to :habitat
+    belongs_to :animal
 end
