174af9d5693c1099189c6fe5e4b9ede3c0a792b2
Nicola Milne <nicolalouisemilne@gmail.com>
2022-07-11 17:52:54 -0600

    created basic endpoints for restaurant and menu data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/restaurants" do
+    restaurants = Restaurant.all.order(:name)
+    restaurants.to_json(include: :ratings)
   end
+
+  get"/restaurants/:id" do
+    restaurant = Restaurant.find(params[:id])
+    restaurant.to_json
+  end
+
+  get"/restaurants/:id/menu" do
+    menu = Restaurant.find(params[:id]).menu_items
+    menu.to_json(include: :item)
+  end
+
 
 end
