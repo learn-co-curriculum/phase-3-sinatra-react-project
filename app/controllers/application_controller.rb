fb0555b0b38f72a831be135b0cddc01f45c31e2b
Thomas Cook <tlncook85@gmail.com>
2022-07-11 19:57:58 -0600

    updating gemlock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/allMovies" do
+    { move: "Donnie Darko"}.to_json
+  end
+
 end
