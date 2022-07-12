1323b8b82c6a8f2aa466e43e4711fce9598adec7
Nicola Milne <nicolalouisemilne@gmail.com>
2022-07-12 11:18:17 -0600

    added serializer method to Restaurant model in order to add average rating to restaurant payload

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,7 +6,17 @@ class Restaurant < ActiveRecord::Base
     belongs_to :cuisine
 
     def average_rating
-        totalratings = self.ratings.sum(:rating)
-        totalratings / self.ratings.length
+        totalratings = self.ratings.sum(:rating) / self.ratings.length
+    end
+
+    def serialize_restaurant
+        attributes_hash = self.attributes
+        attributes_hash.merge!("rating" => self.average_rating)
+    end
+
+    def self.serialize_restaurants
+        self.all.order(:name).map do |restaurant|
+            restaurant.serialize_restaurant
+        end
     end
 end 
