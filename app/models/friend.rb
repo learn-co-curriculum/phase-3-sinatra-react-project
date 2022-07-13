becaec261a7489b524660b7be80cca0c00332d3f
Lyn <lynfinman@gmail.com>
2022-07-13 12:09:03 -0500

    added followers & following flag to users/:id

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,12 @@
 class Friend < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
+
+  # def self.following(id)
+  #   select { |friend| friend.follower_id == id }.map { |f| f.followee }
+  # end
+
+  def self.following(user)
+    select { |friend| friend.follower == user }.map { |f| f.followee }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
   has_many :followees, through: :followers
   has_many :followees, foreign_key: :followee_id, class_name: 'Friend'
   has_many :followers, through: :followees
+
+  def following
+    Friend.following(self)
+  end
 end
