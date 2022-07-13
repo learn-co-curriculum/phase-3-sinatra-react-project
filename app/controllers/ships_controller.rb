fa44332d86a8e68a44402a4397127cb291f8f6de
Devin Garcia <devingarcia5423@gmail.com>
2022-07-13 13:28:09 -0400

    Revert "fixed post and patch functionality"

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,43 +1,40 @@
 class ShipsController < ApplicationController
-
-    get "/ships" do
+  
+    get "/ships" do 
         Ship.all.to_json #(get_ship_json_config)
     end
-
-    get "/ships/:id" do
+    
+    get "/ships/:id" do 
         find_ship
         @ship.to_json
       end
 
-    post "/ships" do
-      ship = Ship.create(name: params[:name], character1_id: params[:character1_id], character2_id: params[:character2_id])
+    post "/ships" do 
+      ship = Ship.create(ship_params)
       ship.to_json
     end
-
-    patch "/ships/:id" do
+  
+    patch "/ships/:id" do 
       find_ship
-      @ship.update(name: params[:name], character1_id: params[:character1_id], character2_id: params[:character2_id])
+      @ship.update(ship_params)
       @ship.to_json
     end
-
-    delete "/ships/:id" do
+     
+    delete "/ships/:id" do 
       find_ship
       @ship.destroy
-      @ship.to_json
     end
-
-    private
+  
+    private 
 
     def find_ship
         @ship = Ship.find(params[:id])
     end
 
-    # def ship_params
-
-    #   params.permit(:character1_id, :character2_id, :ship_name)
-    #   binding.pry
-    # end
-
+    def ship_params
+      params.permit(:character1_id, :character2_id, :ship_name)
+    end
+  
     def get_ship_json_config()
       {
         methods: [:formatted_time]
