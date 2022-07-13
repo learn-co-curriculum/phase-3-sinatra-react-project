ad29ae0a939ca9838e7494dd377e1da79e270c57
Devin Garcia <devingarcia5423@gmail.com>
2022-07-13 13:32:15 -0400

    fixed post and patch functionality for shipcontrol

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,31 +1,33 @@
 class ShipsController < ApplicationController
-  
-    get "/ships" do 
-        Ship.all.to_json #(get_ship_json_config)
-    end
-    
-    get "/ships/:id" do 
-        find_ship
-        @ship.to_json
-      end
 
-    post "/ships" do 
-      ship = Ship.create(ship_params)
-      ship.to_json
-    end
-  
-    patch "/ships/:id" do 
-      find_ship
-      @ship.update(ship_params)
-      @ship.to_json
-    end
-     
-    delete "/ships/:id" do 
-      find_ship
-      @ship.destroy
-    end
-  
-    private 
+  get "/ships" do
+    Ship.all.to_json #(get_ship_json_config)
+  end
+
+  get "/ships/:id" do
+    find_ship
+    @ship.to_json
+  end
+
+  post "/ships" do
+    ship = Ship.create(name: params[:name], character1_id: params[:character1_id], character2_id: params[:character2_id])
+    ship.to_json
+  end
+
+  patch "/ships/:id" do
+    find_ship
+    @ship.update(name: params[:name], character1_id: params[:character1_id], character2_id: params[:character2_id])
+    @ship.to_json
+  end
+
+  delete "/ships/:id" do
+    find_ship
+    @ship.destroy
+    @ship.to_json
+  end
+
+
+  private
 
     def find_ship
         @ship = Ship.find(params[:id])
@@ -34,7 +36,7 @@ class ShipsController < ApplicationController
     def ship_params
       params.permit(:character1_id, :character2_id, :ship_name)
     end
-  
+
     def get_ship_json_config()
       {
         methods: [:formatted_time]
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,6 @@
 class Ship < ActiveRecord::Base
-    belongs_to :character1
-    belongs_to :character2
+  belongs_to :character1
+  belongs_to :character2
+  has_many :comments
+
 end
