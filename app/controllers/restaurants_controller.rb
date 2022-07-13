175768706664a1e8b14e1a9464e73aa0fd766862
Lyn <lynfinman@gmail.com>
2022-07-13 17:14:03 -0500

    returning restaurants reviwed by specific user(s)

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,17 +22,31 @@ class RestaurantsController < ApplicationController
     @restaurant.to_json({ include: :reviews, methods: :average_score })
   end
 
+  get '/restaurants/user/:id' do
+    find_restaurants
+    methods = {}
+    methods.merge!({ include: :reviews }) if params.include?('include_review')
+    methods.merge!({ methods: :average_score })
+    @restaurants.to_json(methods)
+  end
+
   post '/restaurants' do
     restaurant = Restaurant.create(restaurant_params)
     restaurant.to_json
   end
 
   private
-    def restaurant_params
-        allows_params = %(name description category img)
-        params.select {|k,v| allowed_params.include(k)}
-    end
-    def find_restaurant
-        @restaurant = Restaurant.find(params[:id])
-    end
+
+  def restaurant_params
+    allows_params = %(name description category img)
+    params.select { |k, _v| allowed_params.include(k) }
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def find_restaurants
+    @restaurants = params.include?('friend_reviewed') ? Restaurant.reviewed_by(params[:id]) : Restaurant.reviewed_by_friends(params[:id])
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,17 @@ class Restaurant < ActiveRecord::Base
   def average_score
     reviews.average(:score).to_f.round(2)
   end
+
+  def self.reviewed_by(user_id)
+    # Review.where(user_id: user_id).map { |r| r.restaurant }
+    select { |r| r.reviews.where(user_id: user_id).count > 0 }
+  end
+
+  def self.reviewed_by_friends(user_id)
+    reviewed_restaurants = []
+    User.find(user_id).following.each do |friend|
+      reviewed_restaurants |= reviewed_by(friend.id)
+    end
+    reviewed_restaurants
+  end
 end
