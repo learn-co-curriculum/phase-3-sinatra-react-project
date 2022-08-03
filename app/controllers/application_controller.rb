a6896d168f0fd124d5788ac26b2ae910ddefc6fd
mio kataoka <mio.kanna@gmail.com>
2022-08-02 18:57:07 -0700

    added order routing to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,27 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  get "/dogs" do
-    dogs = Dog.all
-    dogs.to_json
+  get "/orders" do
+    orders = Order.all.order(:pickup_date)
+    orders.to_json
   end
-  
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  post "/orders" do
+    order = Order.create(dog: params[:dog], item: params[:item], quantity: params[:quantity], pickup_date: params[:pickup_date])
+    order.to_json
   end
+
+  patch "/orders/:id" do
+    order = Order.find(params[:id])
+    order.update(dog: params[:dog], item: params[:item], quantity: params[:quantity], pickup_date: params[:pickup_date] )
+    order.to_json
+  end
+
+  delete "/orders/:id" do
+    order = Order.find(params[:id])
+    order.destroy
+    order.to_json
+  end
+
 
 end
