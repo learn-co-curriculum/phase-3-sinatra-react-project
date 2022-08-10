9ee57946ef07e4a75846d1aaa0f323417640d6c1
mio kataoka <mio.kanna@gmail.com>
2022-08-10 14:13:42 -0700

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,31 @@ class ApplicationController < Sinatra::Base
   get "/dogs" do
     dogs = Dog.all.order(:name)
     dogs.to_json(include: [:orders])
+  end
 
+  get "/dogs/:id" do
+    dog = Dog.find(params[:id])
+    dog.to_json(include: [:orders])
+  end
+
+  delete "/dogs/:id" do
+    dog = Dog.find(params[:id])
+    dog.destroy
   end
 
   post "/dogs" do
-    order = Dog.create(name: params[:name], breed: params[:breed], age: params[:age], weight: params[:weight])
-    dogs.to_json
+    dog = Dog.create(name: params[:name], breed: params[:breed], age: params[:age], weight: params[:weight])
+    dog.to_json
   end
-
-  
   
   get "/orders" do
     orders = Order.all.order(:pickup_date)
     orders.to_json
+  end
+
+  get "/orders/:id" do
+    order = Order.find(params[:id])
+    order.to_json
   end
 
   post "/orders" do
@@ -26,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/orders/:id" do
     order = Order.find(params[:id])
-    order.update(dog_id: params[:dog_id], item: params[:item], quantity: params[:quantity], pickup_date: params[:pickup_date] )
+    order.update(item: params[:item], quantity: params[:quantity], pickup_date: params[:pickup_date] )
     order.to_json
   end
 
