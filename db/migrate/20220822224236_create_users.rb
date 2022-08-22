3c923820d006df70a54455f535166be471de4db8
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-08-22 17:02:42 -0600

    user table created

--- db/migrate/20220822224236_create_users.rb
+++ db/migrate/20220822224236_create_users.rb
@@ -1,4 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
+  def change 
+    create_table :users to |t|
+    t.string :first_name
+    t.string :last_name
+    t.string :jedi_or_sith
+    t.string :jedi_rank
+    t.string :sith_rank
+    t.string :species
+    t.sting :image
+  
+
   end
 end
