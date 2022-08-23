5e38da9c3cfddecb76a4932d5d3702dc10cc7453
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-08-23 10:17:40 -0600

    migration issued fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  belongs_to: :lightsaber
-  has_many: :lightsaberColors, through: :lightsaber 
-  has_many: :lightsaberStyles , through: :lightsaber
-  has_many: :lightsaberVariations, through: :lightsaber
+  belongs_to :lightsaber
+  has_many :lightsaberColors, through: :lightsaber 
+  has_many :lightsaberStyles , through: :lightsaber
+  has_many :lightsaberVariations, through: :lightsaber
 end
