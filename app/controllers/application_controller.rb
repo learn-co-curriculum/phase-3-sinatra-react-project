39e2681cb56b106db1d42115ee536d8545832e67
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-22 23:23:30 -0700

    implemented all the GET methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,29 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  # if one wants the adopted/non-adopted animals: add query param => /animals?adopted=true[or false]
+  get "/animals" do
+    animals = Animal.all
+    adopted = params[:adopted]
+    if adopted != nil
+      animals = animals.select { 
+        |animal| adopted == "true" ? animal.adopted? : !animal.adopted? 
+      }
+    end
+    animals_details = animals.map do |animal|
+      Animal.get_detail(animal.id)
+    end
+    animals_details.to_json
+  end
+
+  get "/animals/:id" do
+    animal = Animal.get_detail(params[:id])
+    animal.to_json
+  end
+
+  get "/animals/:id/comments" do
+    animal = Animal.find(params[:id])
+    animal.comments.to_json
+  end
+
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,4 +3,12 @@ class Animal < ActiveRecord::Base
     has_many :donations
     has_many :comments
     has_many :likes
+
+    def self.get_detail (id)
+        animal = self.find(id)
+        animal_likes = animal.likes.size
+        animal_total_donations = animal.donations.sum { |donation| donation.amount }
+        result = {**animal.attributes, "likes" => animal_likes, "donations" => animal_total_donations}
+        return result
+    end
 end
