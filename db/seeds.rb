1e234ad026cfff25741abf9662d26ee6e09de007
Nick Warren <nickwarren47@gmail.com>
2022-08-23 09:28:33 -0600

    Added lightsaber color data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,50 @@
-puts "🌱 Seeding spices..."
+puts "Seeding Jedi and Sith...May the force be with you..."
 
-# Seed your database here
+
+User.create(first_name: "Mace", last_name: "Windu", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186039808-cc918735-752e-4a50-9835-6219bbc251fd.jpg" )
+User.create(first_name: "Yoda", last_name: nil, aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Grand Master", sith_rank: nil, species: "Unknown", image_url: "https://user-images.githubusercontent.com/106715328/186040196-87b2cffb-8b4b-4676-a16e-5d6f9a256605.png")
+User.create(first_name: "Plo", last_name: "Koon", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Kel Dor", image_url: "https://user-images.githubusercontent.com/106715328/186048964-03750c49-ab60-46aa-9f4f-9e67db8c78b8.jpg")
+User.create(first_name: "Kit", last_name: "Fisto", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Nautolan", image_url: "https://user-images.githubusercontent.com/106715328/186049275-8ce3ce5f-0507-4ea4-b0e5-400e6a9ed68d.jpg")
+User.create(first_name: "Darth", last_name: "Tyranus", aliases: "Count Dooku", jedi_or_sith: "Sith", jedi_rank: "Master", sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186049532-662ba33a-7c1b-468d-be6f-9ab5738c5d89.jpg")
+User.create(first_name: "Qui-Gon", last_name: "Jinn", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186049692-6d8e37fe-ec4f-4407-84ce-9bf8c18890b9.jpg")
+User.create(first_name: "Obi-Wan", last_name: "Kenobi", aliases: "Ben Kenobi", jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186049975-de1d4d43-f28e-4ae9-9ae3-b2275cdf004d.jpg")
+User.create(first_name: "Anakin", last_name: "Skywalker", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Knight", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186050194-50fc8c20-c030-48b6-8c0f-29f4d5f1bd93.jpg")
+User.create(first_name: "Darth", last_name: "Vader", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186050312-ee7bd9a2-2f30-4a02-bd74-e0d9a8056073.jpg")
+User.create(first_name: "Ahsoka", last_name: "Tano", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Padawan", sith_rank: nil, species: "Togruta", image_url: "https://user-images.githubusercontent.com/106715328/186050599-b54085e4-ddb8-4e6d-aadf-6296f8f703d3.jpg")
+User.create(first_name: "Cal", last_name: "Kestis", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Padawan", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186050851-f811e0dd-4e6d-41e5-90cd-36abc54ac9aa.jpg")
+User.create(first_name: "Kanan", last_name: "Jarrus", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Padawan", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186051077-32330127-d1df-4cee-963e-7a86ffe17645.jpg")
+User.create(first_name: "Ezra", last_name: "Bridger", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Padawan", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186051222-62e6ffd0-527c-4653-8dec-92d9db7201ed.jpg")
+User.create(first_name: "Luke", last_name: "Skywalker", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186051361-c5f4f52d-adc4-4e3e-aafb-73302a1fe6f9.jpg")
+User.create(first_name: "Leia", last_name: "Organa", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Padawan", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186051515-6af8a0df-a3f5-43cd-8d4e-5c2e67ba6ddc.jpg")
+User.create(first_name: "Grogu", last_name: nil, aliases: "Baby Yoda", jedi_or_sith: "Jedi", jedi_rank: "Youngling", sith_rank: nil, species: "Unknown", image_url: "https://user-images.githubusercontent.com/106715328/186051778-2f660d5c-e11e-474d-a825-b5ca279eadaa.jpg")
+User.create(first_name: "Kylo", last_name: "Ren", aliases: "Ben Solo", jedi_or_sith: "Sith", jedi_rank: "Padawan", sith_rank: "Accolite", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186051955-2d79a17b-2898-49af-a9e2-a01d7b56f949.jpg")
+User.create(first_name: "Rey", last_name: "Skywalker", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Master", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186052159-2caa47db-74e3-4556-bb1b-5f9a4b19293d.jpg")
+User.create(first_name: "Darth", last_name: "Maul", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Master", species: "Dathomirian", image_url: "https://user-images.githubusercontent.com/106715328/186052334-3a2af11b-5bd7-4019-b971-31b57f1c8939.jpg")
+User.create(first_name: "Darth", last_name: "Nihilus", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186052501-0fc82e2b-484f-4de5-87fb-32d9d9bb69c6.jpg")
+User.create(first_name: "Darth", last_name: "Sidious", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186052631-a286b20e-bc62-4be7-82f4-463cf196e850.jpg")
+User.create(first_name: "Darth", last_name: "Plagueis", aliases: "Darth Plagueis the Wise", jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Muun", image_url: "https://user-images.githubusercontent.com/106715328/186052832-cd805798-14da-49ef-bed2-d0da7d2b5d51.jpg")
+User.create(first_name: "Darth", last_name: "Bane", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186053007-f47088fa-f001-436f-8feb-687293e55139.jpg")
+User.create(first_name: "Darth", last_name: "Zannah", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Accolite", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186053114-9327ff87-cb65-4e37-b3b2-a877cbe397eb.jpg")
+User.create(first_name: "Darth", last_name: "Revan", aliases: nil, jedi_or_sith: "Sith", jedi_rank: "Master", sith_rank: "Lord", species: "Force Ghost", image_url: "https://user-images.githubusercontent.com/106715328/186053271-1f72064b-4168-46af-b354-cba7fc502a9e.jpg")
+User.create(first_name: "Darth", last_name: "Sion", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Crusader", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186053485-3beeedf9-91c5-4759-95d8-c4acafb5ded6.jpg")
+User.create(first_name: "Darth", last_name: "Traya", aliases: "Kreia", jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186053632-728b79c4-17e7-4704-a08a-1ca017a334c8.jpg")
+User.create(first_name: "Darth", last_name: "Caedus", aliases: "Jacen Solo", jedi_or_sith: "Sith", jedi_rank: "Knight", sith_rank: "Accolite", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186053851-411f50ba-c7be-4646-ba92-53296611fed6.jpg")
+User.create(first_name: "Darth", last_name: "Krayt", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186054399-dd924c15-ea60-4f2d-a0cf-67373747c9d1.jpg")
+User.create(first_name: "Bastila", last_name: "Shan", aliases: nil, jedi_or_sith: "Jedi", jedi_rank: "Knight", sith_rank: nil, species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186054973-f72dbc0a-1466-482f-97c4-fb76af04e452.jpg")
+User.create(first_name: "Darth", last_name: "Chratis", aliases: nil, jedi_or_sith: "Sith", jedi_rank: nil, sith_rank: "Lord", species: "Human", image_url: "https://user-images.githubusercontent.com/106715328/186055357-84bff90d-f553-48ec-9341-3462d03f4f11.jpg")
+
+puts "Seeding lightsaber colors..."
+LightsaberColor.create(image_url: "https://user-images.githubusercontent.com/106715328/186197264-74e76b37-f5e1-4a84-a919-add0d70afef9.jpg", attribute: , force_users: , crystal_color: "white")
+LightsaberColor.create(image_url: "https://user-images.githubusercontent.com/106715328/186198956-c5d55d59-f20d-4063-971b-e217a963eb6c.jpg", attribute: , force_users: , crystal_color: "blue")
+LightsaberColor.create(image_url: "https://user-images.githubusercontent.com/106715328/186197458-f5bd09e2-b33b-42f9-838a-513c35a63c69.jpg" , attribute: , force_users: , crystal_color: "yellow-orange")
+LightsaberColor.create(image_url: "https://user-images.githubusercontent.com/106715328/186198868-2d8eff94-d57a-45f6-8261-91f7afe24e38.jpg", attribute: , force_users: , crystal_color: "green")
+LightsaberColor.create(image_url: "https://user-images.githubusercontent.com/106715328/186198697-1bf957a4-2fa2-4fe7-911e-693b8b607374.jpg", attribute: , force_users: , crystal_color: "red")
+LightsaberColor.create(image_url: "https://user-images.githubusercontent.com/106715328/186198779-57996478-5c25-482f-b352-ca544fa8b2e0.jpg", attribute: , force_users: , crystal_color: "purple")
+
+
+
+
+
+
 
 puts "✅ Done seeding!"
