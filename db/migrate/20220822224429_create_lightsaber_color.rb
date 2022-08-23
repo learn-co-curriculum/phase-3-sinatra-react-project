09f72c96247dacb2fad9b95bf832f23e9d21245b
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-08-23 10:40:21 -0600

    seeding issue fixed

--- db/migrate/20220822224429_create_lightsaber_color.rb
+++ db/migrate/20220822224429_create_lightsaber_color.rb
@@ -2,7 +2,7 @@ class CreateLightsaberColor < ActiveRecord::Migration[6.1]
   def change
     create_table :lightsaber_colors do |t|
       t.string :image_url
-      t.string :attribute 
+      t.string :symbolism 
       t.string :force_users
       t.string :crystal_color
     end
