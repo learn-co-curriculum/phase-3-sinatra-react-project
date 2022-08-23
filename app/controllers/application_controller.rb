ca8cef9ee8bc83aaeacdd214f54304dbf01773f0
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-23 12:07:59 -0700

    added GET, POST, PATCH, and DELETE methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
 
   # if one wants the adopted/non-adopted animals: add query param => /animals?adopted=true[or false]
   get "/animals" do
@@ -29,6 +24,70 @@ class ApplicationController < Sinatra::Base
   get "/animals/:id/comments" do
     animal = Animal.find(params[:id])
     animal.comments.to_json
+  end
+
+  get "/animals/:id/likes" do
+    animal = Animal.find(params[:id])
+    animal.likes.to_json
+  end
+
+  post "/animals/:id/comments" do
+    comment = Comment.create(
+      message: params[:message],
+      user_id: params[:user_id],
+      animal_id: params[:id]
+    )
+    comment.to_json
+  end
+
+  post "/animals/:id/likes" do
+    like = Like.create(
+      animal_id: params[:id],
+      user_id: params[:user_id]
+    )
+    like.to_json
+  end
+
+  post "/users" do
+    user = User.create(
+      name: params[:name],
+      username: params[:username],
+      password: params[:password],
+      location: params[:location]
+    )
+    user.to_json
+  end
+
+  post "/animals/:id/donations" do
+    donation = Donation.create(
+      amount: params[:amount],
+      animal_id: params[:id],
+      user_id: params[:user_id]
+    )
+    donation.to_json
+  end
+
+  # forego the "?" from the adopted when making a request.
+  patch "/animals/:id" do
+    animal = Animal.find(params[:id])
+    animal.update(
+      adopted?: params[:adopted],
+      user_id: params[:user_id]
+    )
+    animal.to_json
+  end
+
+  delete "/comments/:id" do
+    comment = Comment.find(params[:id])
+    comment.destroy
+    comment.to_json
+  end
+
+  # Sinatra does NOT accept a body for delete method, but it accepts query parameters.
+  delete "/animals/:id/likes" do
+    like = Like.find_by(user_id: params[:user_id], animal_id: params[:id])
+    like.destroy
+    like.to_json
   end
 
 end
