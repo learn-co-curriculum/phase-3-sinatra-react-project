446dd7b8c69893d74c6c235a768b3f5ec115c986
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-09-13 15:41:55 -0500

    added days/id

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -3,4 +3,9 @@ class DaysController < ApplicationController
         days = Day.all 
         days.to_json
     end
+
+    get '/days/:id' do 
+        day = Day.find(params[:id])
+        day.to_json
+    end
 end
