d49e7f6f84bfede91fbc0a6c54effbf8879a96d3
Wendy You <wyou130@gmail.com>
2022-09-13 14:05:56 -0400

    finalize backend

--- app/models/Band.rb
+++ app/models/Band.rb
@@ -1,2 +1,6 @@
 class Band < ActiveRecord::Base
+    has_many :concerts
+    has_many :cities, through: :concerts
+    has_many :eateries, through: :cities
+    
 end
--- app/models/Eatery.rb
+++ app/models/Eatery.rb
@@ -1,5 +1,5 @@
 class Eatery < ActiveRecord::Base
   belongs_to :city
-  has_many :concerts, through: :cities
-  has_many :bands, through :concerts
+  has_many :concerts, through: :city
+  has_many :bands, through: :concerts
 end
