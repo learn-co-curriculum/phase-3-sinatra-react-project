2b181f67620b398d53c287a155d7b1fe03f96386
jake dapper <jakedapper@gmail.com>
2022-09-13 10:46:12 -0500

    setting up routes, initial states

--- trip-planner-frontend/src/components/App.js
+++ trip-planner-frontend/src/components/App.js
@@ -1,20 +1,41 @@
 import '../App.css';
+import React, {useState} from 'react'
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import Home from './Home.js'
 import NavBar from './NavBar.js';
-import List from './List.js';
-import ListItem from './ListItem.js';
+import AttractionList from './AttractionList.js';
 import AttractionsForm from './AttractionsForm.js';
-import FilterSort from './FilterSort.js';
 
 function App() {
+
+  const [chosenCities, setChosenCities] = useState([])
+  const [bandsPlaying, setBandsPlaying] = useState([])
+  const [chosenEateries, setChosenEateries] = useState([])
+  const [userPrefs, setUserPrefs] = useState([])
+
+  function resetChoices(){
+    setChosenCities([])
+    setBandsPlaying([])
+    setChosenEateries([])
+  }
+
+
   return (
     <div className="App">
-      <NavBar/>
-      <Home/>
-      <List/> 
-      <ListItem/>
-      <AttractionsForm/>
-      <FilterSort/>
+      <Switch>
+        <Route>
+          <NavBar resetChosen={resetChoices}/>
+        </Route>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/attractions">
+          <AttractionList/> 
+        </Route>
+        <Route exact path="/addAttraction">
+          <AttractionsForm/>
+        </Route>
+      </Switch>
     </div>
   );
 }
