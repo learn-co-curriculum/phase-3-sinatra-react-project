649e3ebb4a5712fb939e33396399ee56f42412cb
Daniel Magers <daniel.magers@gmail.com>
2022-09-12 19:02:41 -0500

    initial setup'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
+
+  get "/hello" do
+      '<h2>Hello <em>World</em>!</h2>'
+end
 
 end
+
+
+# get "/" do
+
+# { message: "Good luck with your project!" }.to_json
+# end
