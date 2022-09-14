6cf2debf9cb560d2386ec8329c256124aedc807e
Wendy You <wyou130@gmail.com>
2022-09-14 15:30:34 -0400

    cities dropdown

--- trip-planner-frontend/src/components/AttractionList.js
+++ trip-planner-frontend/src/components/AttractionList.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ConcertItem from './ConcertItem.js';
 import EateryItem from './EateryItem.js';
 
 
-function AttractionList({availableCities, displayData, attractionType}) {
-    // console.log(displayData)
+function AttractionList({availableCities, displayData, attractionType, onHandleChange, dropdownValue}) {
+
+
+
+    function handleChange(e) {
+        // console.log(e.target.value)
+        onHandleChange(e.target.value)
+    }
+
     return (
         <div>
-            <select>
+            <select value={dropdownValue} onChange={handleChange}>
                 <option>All</option>
-                {availableCities.map((city)=> <option key={city.id} value={city.name}>{city.name}</option>)}
+                {availableCities.map((city)=> <option key={city.id} value={city.id}>{city.name}</option>)}
             </select>
             <div>
                 {attractionType === "concerts" ? <h3>Concerts</h3> : <h3>Eateries</h3>}
