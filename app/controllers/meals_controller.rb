d1a26f4f8435957d6f50168a490e6d7439b66098
Jaden Lund <lundjaden@gmail.com>
2022-09-15 16:52:37 -0400

    Small styling changes | adding a submission feature for adding your own recipe

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,30 +1,30 @@
 class MealsController < ApplicationController
-    get '/meals' do 
-        meals = Meal.all 
-        meals.to_json
-    end
+  get "/meals" do
+    meals = Meal.all.joins(:day) # (:joins => :days, :conditions => { :days => { :id => day_id } })
+    meals.to_json
+  end
 
-    get '/meals/:id' do 
-        meal = Meal.find(params[:id])
-        meal.to_json
-    end
+  get "/meals/:id" do
+    meal = Meal.find(params[:id])
+    meal.to_json
+  end
 
-    get '/meals/:id/items' do 
-        items = Meal.find(params[:id]).meal_items
-        items.to_json
-    end
+  get "/meals/:id/items" do
+    items = Meal.find(params[:id]).meal_items
+    items.to_json
+  end
 
-    patch '/meals/:id' do 
-        meal = Meal.find(params[:id])
-        meal.update(day_id: params[:day_id], meal_number: params[:meal_number])
-        meal.to_json
-    end
+  patch "/meals/:id" do
+    meal = Meal.find(params[:id])
+    meal.update(day_id: params[:day_id], meal_number: params[:meal_number])
+    meal.to_json
+  end
 
-    delete '/meals/:id' do 
-        meal = Meal.find(params[:id])
-        items = meal.meal_items 
-        items.destroy_all
-        meal.destroy
-        status 204
-    end
+  delete "/meals/:id" do
+    meal = Meal.find(params[:id])
+    items = meal.meal_items
+    items.destroy_all
+    meal.destroy
+    status 204
+  end
 end
