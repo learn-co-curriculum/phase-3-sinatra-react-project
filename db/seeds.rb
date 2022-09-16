3582ebe2ee5478846da827e8c6581ea517f5c70d
Peyton Harrison <peyton006@hotmail.com>
2022-09-15 20:26:08 -0500

    final push & bug fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,13 @@ a5 = Artist.create(name: "Amigo The Devil", likes: true, image_url: "https://ima
 g1 = Genre.create(title: "Pop Rock", favorite: true, top3: true)
 g2 = Genre.create(title: "Pop", favorite: true, top3: true)
 g3 = Genre.create(title: "Rap", favorite: false, top3: false)
-g4 = Genre.create(title: "Rap", favorite: false, top3: false)
+g4 = Genre.create(title: "Alternative", favorite: false, top3: false)
 g5 = Genre.create(title: "Murder Folk", favorite: true, top3: true)
 
 s1 = Song.create(artist_id: a1.id, genre_id: g1.id, song_title: "As it Was", release_date: "April 1, 2022")
 s2 = Song.create(artist_id: a2.id, genre_id: g2.id, song_title: "Running Up That Hill", release_date: "August 5, 1985")
 s3 = Song.create(artist_id: a3.id, genre_id: g3.id, song_title: "Staying Alive", release_date: "August 5, 2022")
-s4 = Song.create(artist_id: a4.id, genre_id: g4.id, song_title: "First Class", release_date: "April 8, 2022")
+s4 = Song.create(artist_id: a4.id, genre_id: g3.id, song_title: "First Class", release_date: "April 8, 2022")
 s5 = Song.create(artist_id: a5.id, genre_id: g5.id, song_title: "Hell and You", release_date: "June 16, 2015")
 
 puts "✅ Done seeding!"
