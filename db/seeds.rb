f1827a3d98a6d056d2868025a221f7b425632643
Pete Fowler <fowler.pete@gmail.com>
2022-09-30 11:28:00 -0600

    Add rest-client gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+require 'rest-client' 
+
 puts "🌱 Seeding spices..."
 
-response = RestClient.get "https://api.yelp.com/v3/businesses/{id}"
+response = RestClient.get "https://api.yelp.com/v3/businesses/search"
 
 puts "✅ Done seeding!"
