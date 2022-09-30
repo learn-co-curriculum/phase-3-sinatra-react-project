5c3a192c7a8c0c03f01ad61dd5866bce175361cb
Pete Fowler <fowler.pete@gmail.com>
2022-09-30 11:41:17 -0600

    Add headers to get

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ require 'rest-client'
 
 puts "🌱 Seeding spices..."
 
-response = RestClient.get "https://api.yelp.com/v3/businesses/search"
+response = RestClient.get("https://api.yelp.com/v3/businesses/search?location=denver", 
+  headers={
+    "Authorization": ENV['API_KEY']
+  })
 
 puts "✅ Done seeding!"
