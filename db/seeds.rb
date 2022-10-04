5d2fd0bbb6d2ab789304fc235e00e6a426da1527
ChaseWelsh <fseusb@gmail.com>
2022-10-04 13:39:11 -0600

    Add Phone Number and Website to Businesses

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,16 @@ response = RestClient.get(url, headers={Authorization: ENV['API_KEY']})
 
 businesses = JSON.parse(response)["businesses"]
 
+Faker::Config.locale = 'en-US'
 businesses.each do |business|
   Business.create(
     name: business["name"],
     business_type: business["categories"][0]["title"],
     address: business["location"]["display_address"].join(', '),
     price: business["price"],
-    image_url: business["image_url"]
+    image_url: business["image_url"],
+    phone_number: "(#{Faker::PhoneNumber.area_code}) #{Faker::PhoneNumber.exchange_code}-#{Faker::PhoneNumber.subscriber_number(length: 4)}",
+    website: Faker::Internet.url
   )
 end
 
