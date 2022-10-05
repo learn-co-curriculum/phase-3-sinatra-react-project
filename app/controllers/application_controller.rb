171e3546d196200b4716b252143bbce9f949a7ad
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-10-05 19:07:19 -0400

    added new routes, organized, and methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,33 @@ class ApplicationController < Sinatra::Base
     place = Place.all
     place.to_json(include: { reviews: { include: :user }})
   end
+  #returns a specific instance
+  get "/places/:id" do
+    place = Place.find(params[:id])
+    place.to_json(include: { reviews: { include: :user }})
+  end
+  #deletes place
+  delete "/places/:id" do 
+    place = Place.find(params[:id])
+    place.destroy
+    place.to_json
+  end
+  #updates price of place
+  patch "/places/:id" do
+    place = Place.find(params[:id]).to_json
+    place.update(price:params[:price])
+  end
+  #grabs all the places by category
+  get "/places/category_sort/:category" do
+    category = Place.category_sort(params[:category])
+    category.to_json
+  end
+
+  #post request from input form
+  post '/places' do
+    place = Place.create(name:params[:name], category:params[:category], price:params[:price], location:params[:location], image_url:params[:image_url])
+    place.to_json
+  end
   #returns all users
   get "/users" do
     user = User.all
@@ -16,27 +43,6 @@ class ApplicationController < Sinatra::Base
     review = Review.all
     review.to_json
   end
-  #returns a specific instance
-  get "/places/:id" do
-    place = Place.find(params[:id])
-    place.to_json(include: { reviews: { include: :user }})
-  end
-  #grabs all the 
-  get "/places/category_sort/:category" do
-    category = Place.category_sort(params[:category])
-    category.to_json
-  end
-
-#post request from input form
-  post '/places' do
-    place = Place.create(name:params[:name], category:params[:category], price:params[:price], location:params[:location], image_url:params[:image_url])
-    place.to_json
-  end
 
 
-  delete "/places/:id" do 
-    place = Place.find(params[:id])
-    place.destroy
-    place.to_json
-  end
 end
