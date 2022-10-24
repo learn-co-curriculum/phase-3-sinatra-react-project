892470d99b1ef096905f802e981141da85584217
Cindy Qu <cindyqu42@gmail.com>
2022-10-24 10:31:28 -0700

    updated get requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  #Read
+  # get "/" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
+
+  get "/api/users" do
+    users = User.all 
+    users.to_json(include: :songs)
   end
+
+  get "/api/users/:id" do
+    users = User.find(params[:id])
+    users.to_json(include: :songs)
+  end
+
 
 end
