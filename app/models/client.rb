83a8ab99b5eff084546650079c97c3f5f97e8998
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-24 15:43:12 -0400

    basic crud routes added for backend

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,7 @@
 class Client < ActiveRecord::Base
     belongs_to :therapist
+
+    def full_name 
+        "#{self.first_name} #{self.last_name}"
+    end
 end
--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,3 +1,7 @@
 class Therapist < ActiveRecord::Base
     has_many :clients
+
+    def full_name 
+        "#{self.first_name} #{self.last_name}"
+    end
 end
