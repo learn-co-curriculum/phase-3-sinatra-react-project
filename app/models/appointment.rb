39f1fa42948fcbd428ff580e3b2c847c16c83399
Daniel Boley <danboley1@gmail.com>
2022-10-25 11:21:31 -0400

    added post, patch, and delete requests to application controller

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,9 +3,4 @@ class Appointment < ActiveRecord::Base
     belongs_to :tutor
     belongs_to :student
 
-    def new_appt(tutor, student, date, time, location, notes)
-        Appointment.create(tutor_id: tutor.id, student_id: student.id, date: date, time: time, location: location, notes: notes)
-    end
-
-
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -3,8 +3,4 @@ class Tutor < ActiveRecord::Base
     has_many :appointments
     has_many :students, through: :appointments
 
-    def new_tutor(first_name, last_name, location, subject)
-        Tutor.create(first_name: first_name, last_name: last_name, location: location, subject: subject)
-    end
-
 end
