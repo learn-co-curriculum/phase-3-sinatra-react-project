8c59ba91783493458bed354430dd5f45fc48bedd
Daniel Boley <danboley1@gmail.com>
2022-10-25 09:50:39 -0400

    added backend get requests to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,34 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/tutors" do
+    tutors = Tutor.all
+    tutors.to_json
+  end
+
+  get "/tutors/:id" do
+    tutor = Tutor.find(params[:id])
+    tutor.to_json(include: :appointments)
+  end
+
+  get "/pupils" do
+    pupils = Pupil.all
+    pupils.to_json
+  end
+
+  get "/pupils/:id" do
+    pupil = Pupil.find(params[:id])
+    pupil.to_json(include: :appointments)
+  end
+
+  get "/appointments" do
+    appointments = Appointment.all
+    appointments.to_json
+  end
+
+  get "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.to_json
   end
 
 end
