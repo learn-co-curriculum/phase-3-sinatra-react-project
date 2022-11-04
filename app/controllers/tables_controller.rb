b40036d3e5ef31c95cc3527bcfd7f7a9ce143fec
tkyg <80114433+tkyg@users.noreply.github.com>
2022-11-03 21:26:06 -0400

    create CREATE for tables controller

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -10,4 +10,17 @@ class TablesController < ApplicationController
     table.to_json(include: [:guest])
   end
 
+  post "/tables" do
+    table = Table.create(
+      table_number: params[:table_number],
+    )
+    if table.save
+      table.to_json(include: :guest)
+    else
+      { error: table.errors.full_messages }.to_json
+    end
+  end
+
+
+
 end
