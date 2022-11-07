ccb690c6a02d360247e50ec362c28f33c098bbd5
Essyuge <ugeesther@gmail.com>
2022-11-08 01:24:51 +0300

    done

---
+++ front-end/front-end-app/src/components/ProjectsContainer.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import ProjectForm from "./ProjectForm";
+import ProjectList from "./ProjectList";
+import ProjectEditForm from "./ProjectEditForm";
+import ProjectDetail from "./ProjectDetail";
