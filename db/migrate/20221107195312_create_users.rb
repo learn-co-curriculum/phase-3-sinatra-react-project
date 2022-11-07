dc1591ea13fc4286608fdba1fc7fb6bc263b0c13
Essyuge <ugeesther@gmail.com>
2022-11-07 23:02:31 +0300

    done

--- db/migrate/20221107195312_create_users.rb
+++ db/migrate/20221107195312_create_users.rb
@@ -1,4 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
+      t.string :county
+      t.string :city
+      t.string :address
+
+      t.string :password_digest
+
+      t.timestamps
+    end
   end
 end
