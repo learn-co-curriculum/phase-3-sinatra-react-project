85281fd34b5222f3c2615d3264ad796114feabcd
Essyuge <ugeesther@gmail.com>
2022-11-08 01:20:05 +0300

    done

--- front-end/front-end-app/src/App.js
+++ front-end/front-end-app/src/App.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import { Switch, Route } from "react-router-dom"
+import Header from "./components/Header";
+import About from "./components/About";
+import ProjectsContainer from "./components/ProjectsContainer";
 
+import Home from "./components/Home";
 function App() {
   return (
     <App/>
