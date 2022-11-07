ee1d35d2df5ae926c9a5b09556d3689c414eb098
Essyuge <ugeesther@gmail.com>
2022-11-08 01:19:06 +0300

    done

--- front-end/front-end-app/src/index.js
+++ front-end/front-end-app/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
