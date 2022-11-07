05da12ac6e4b0f2b369284f8af5ca24e67a0b549
Essyuge <ugeesther@gmail.com>
2022-11-08 01:19:31 +0300

    done

--- front-end/front-end-app/src/index.js
+++ front-end/front-end-app/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 
