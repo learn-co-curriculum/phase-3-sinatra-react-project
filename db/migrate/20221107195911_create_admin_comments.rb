f50d7a831b9555ec882e83724c6ce3fba277b4d2
Essyuge <ugeesther@gmail.com>
2022-11-07 23:06:27 +0300

    done

--- db/migrate/20221107195911_create_admin_comments.rb
+++ db/migrate/20221107195911_create_admin_comments.rb
@@ -1,4 +1,16 @@
 class CreateAdminComments < ActiveRecord::Migration[6.1]
-  def change
+  def self.up
+    create_table :active_admin_comments do |t|
+      t.string :namespace
+      t.text   :body
+      t.references :resource, polymorphic: true
+      t.references :author, polymorphic: true
+      t.timestamps
+    end
+    add_index :active_admin_comments, [:namespace]
+  end
+
+  def self.down
+    drop_table :active_admin_comments
   end
 end
