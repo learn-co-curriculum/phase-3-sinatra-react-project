d47b6ede9d005e05de4a5337f22fba82ea3a7e22
Essyuge <ugeesther@gmail.com>
2022-11-08 01:25:45 +0300

    done

--- front-end/front-end-app/src/components/ProjectsContainer.js
+++ front-end/front-end-app/src/components/ProjectsContainer.js
@@ -4,3 +4,8 @@ import ProjectForm from "./ProjectForm";
 import ProjectList from "./ProjectList";
 import ProjectEditForm from "./ProjectEditForm";
 import ProjectDetail from "./ProjectDetail";
+
+function ProjectsContainer() {
+    const [projects, setProjects] = useState([]);
+    const [selectedPhase, setSelectedPhase] = useState("");
+    const [searchQuery, setSearchQuery] = useState("");
