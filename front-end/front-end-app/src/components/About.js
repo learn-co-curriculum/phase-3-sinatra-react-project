1aa867bc44819befa420f1c4a01dd8832accdf91
Essyuge <ugeesther@gmail.com>
2022-11-08 00:50:43 +0300

    done

---
+++ front-end/front-end-app/src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+  return (
+    <section className="about">
+      <h1>About</h1>
+      <p>Welcome to Open Library </p>
+      <ul>
+        <li>Read Free Library Books Online</li>
+        <li>Keep Track of your Favourite Books</li>
+        <li>Try Fulltext Search</li>
+
+      </ul>
+    </section>
+  )
+}
+
+export default About
