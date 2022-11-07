bd2e5eae56a889bef84fd1e9a92266e8aafbb2a7
Essyuge <ugeesther@gmail.com>
2022-11-08 01:22:59 +0300

    done

--- front-end/front-end-app/src/components/ProjectListItem.js
+++ front-end/front-end-app/src/components/ProjectListItem.js
@@ -1,3 +1,18 @@
 import { useState } from "react";
 import { FaPencilAlt, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+
+const ProjectListItem = ({
+    project,
+    onProjectEdit,
+    onProjectDelete,
+  }) => {
+    const { id, image, about, name, link, phase } = project;
+  
+    const [clapCount, setClapCount] = useState(0);
+  
+    const handleClap = (clapCount) => setClapCount(clapCount + 1);
+  
+    const handleEditClick = () => {
+      onProjectEdit(project);
+    };
