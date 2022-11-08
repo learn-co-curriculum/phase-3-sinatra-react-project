ffda09d63ef932f6ae4ff9de90bd553c4d071536
Essyuge <ugeesther@gmail.com>
2022-11-08 05:34:07 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,36 +2,49 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
  
-#   get '/books' do
-#     books = Author.all
-#     books.to_json(include: :books)
-# end
+  get '/books' do
+    books = Author.all
+    books.to_json(include: :books)
+end
 
-# get '/books/only' do
-#     books = Book.all
-#     books.to_json
-# end
+get '/books/only' do
+    books = Book.all
+    books.to_json
+end
 
-# post '/books/only' do
-#     book1 = Book.create(
-#         title: params[:title],
-#         thumbnail: params[:thumbnail],
-#         pagecount: params[:pagecount],
-#         author_id: params[:author_id], 
-#     )
-#     book1.to_json
-# end
+post '/books' do
+    book1 = Book.create(
+        title: params[:title],
+        author_id: params[:author_id], 
+        summary: params[:summary],
+        category: params[:category],
+        image: params[:image],
+       
+    )
+    book1.to_json
+end
 
-# delete '/books/only/:id' do
-#     deleted = Book.find(params[:id])
-#     deleted.destroy
-#     deleted.to_json
-# end
+patch '/books/only:id' do
+    book = Book.find(params[:id])
+    book.update(body: params[:body])
+    book.to_json
+  end
+patch '/books/:id' do
+    book = Book.find(params[:id])
+    book.update(body: params[:body])
+    book.to_json
+  end
 
-# delete '/books/:id' do
-#     deleted = Book.find(params[:id])
-#     deleted.destroy
-#     deleted.to_json
-# end
+delete '/books/only/:id' do
+    deleted = Book.find(params[:id])
+    deleted.destroy
+    deleted.to_json
+end
+
+delete '/books/:id' do
+    deleted = Book.find(params[:id])
+    deleted.destroy
+    deleted.to_json
+end
 
 end
