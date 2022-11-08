bb5bd3a81557f22f72bbe418e9089e2d00cb4471
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-08 13:07:18 +0300

    created post request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,15 @@ class ApplicationController < Sinatra::Base
         category = Category.find_by(name: params[:name]).items.to_json(include: :category)
     end
 
-post "/" do 
-
-
-end
+    post '/bucket_list/categories' do
+        category = Category.new(name: params[:name])
+        if category.save
+            category.to_json(include: :items)
+        else
+            {errors: category.errors_array}.to_json
+        end
+    end
+    
 patch "/" do
 
 end
