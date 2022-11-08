17e5c33f5c7afbd557b3371a6373c858272fca4a
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-08 13:25:29 +0300

    created validations for  name,category_id and completed columns  in class item

--- app/models/Item.rb
+++ app/models/Item.rb
@@ -1,3 +1,16 @@
 class Item < ActiveRecord::Base
 belongs_to :category
+
+ validates :name, presence: true
+ validates :category_id, presence: true
+ validates :completed, inclusion: { in: [true, false] }
+
+    def errors_array
+        self.errors.full_messages
+    end
+
+    def self.by_name
+        self.name.downcase
+    end
+
 end  
