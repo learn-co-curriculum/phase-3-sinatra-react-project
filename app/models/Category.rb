60f106e6f7e687f6c9ba4fe884224c4b5cdeca30
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-08 13:22:30 +0300

    created an error-array method in class category

--- app/models/Category.rb
+++ app/models/Category.rb
@@ -1,6 +1,10 @@
 class Category < ActiveRecord::Base
     # category has many to many relationships with items
 has_many :items
- validates :name, presence: true, uniqueness: true
- 
+
+validates :name, presence: true, uniqueness: true
+ def errors_array
+        self.errors.full_messages
+    end
+
 end
