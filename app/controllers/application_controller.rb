119fe77c27b93de94032b92b56d8c6b02d6f4796
kapadokia <ktnyandoch@gmail.com>
2022-11-08 09:53:54 +0300

    starter code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Am Lucky to have a code that works on first instance" }.to_json
   end
 
 end
