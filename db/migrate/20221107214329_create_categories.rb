25ff2252a5579613343614363c172fcf075608bf
Beth Njuguna <libbybetha@gmail.com>
2022-11-08 11:55:03 +0300

    changed data

--- db/migrate/20221107214329_create_categories.rb
+++ db/migrate/20221107214329_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
   def change
     create_table :categories do |t|
       t.string  :name
-      t.boolean  :completed_events
+      t.boolean  :completed_event
     end
   end
 end
