4244cfd36ac795fc5bbb25e4ef4e46e52d9918f7
Beth Njuguna <libbybetha@gmail.com>
2022-11-08 10:57:02 +0300

    changed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,11 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+    travel = Category.create(name: "travel")
+    lifestyle = Category.create(name: "lifestyle")
+    education = Category.create(name: "education")
+
+   
+
+
 puts "✅ Done seeding!"
