9b288151e6af5bbdc90daf882550f142dfafc511
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-11-08 09:39:58 +0300

    enable CORS to allow origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  before do
+    response.headers['Access-Control-Allow-Origin'] = "*"
+  end
+
+  #enable CORS prefLight requests
+  options "*" do
+    response.headers['Access-Control-Allow-Method'] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  end
   
   # Add your routes here
   get "/" do
