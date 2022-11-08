d915f8504ea55d9b90ca6ac9c3db18d226513191
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-08 13:08:48 +0300

    created delete method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,15 @@ class ApplicationController < Sinatra::Base
             {errors: category.errors_array}.to_json
         end
     end
-    
-patch "/" do
+
+  delete '/bucket_list/categories/:name' do
+        category = Category.find_by(name: params[:name])
+        if category
+            category.destroy
+            category.to_json(include: :items)
+        else
+            {errors: "Category not found"}.to_json
+        end
+    end
 
 end
-delete "/" do
-  
-end
-end
- 
