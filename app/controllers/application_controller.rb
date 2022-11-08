33e77276084ae43c859d7ee4de044e41a0c5b639
Mulu <brianmulu007@gmail.com>
2022-11-08 13:30:02 +0300

    Creation of book data in seed.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
+  get "/books" do
     { message: "Good luck with your project!" }.to_json
   end
 
