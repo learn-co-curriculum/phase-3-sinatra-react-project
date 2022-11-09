c58fbf2b959b1bae73e5a49137153686319180b4
RoyOkite <royokite@gmail.com>
2022-11-10 01:50:34 +0300

    updated application controller routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,32 @@ class ApplicationController < Sinatra::Base
     Customer.all.to_json
   end
 
+  get "/customers/:id" do
+    Customer.where(id: params["id"]).first.to_json
+  end
+
   get "/products" do
     Product.all.to_json
   end
 
+  get "/products/:id" do
+    Product.where(id: params["id"]).first.to_json
+  end
+
   get "/orders" do
     Order.all.to_json
+  end
+
+  get "/orders/:id" do
+    Order.where(id: params["id"]).first.to_json
+  end
+
+  get "/order_details" do
+    OrderDetail.all.to_json
+  end
+
+  get "/order_details/:id" do
+    OrderDetail.where(id: params["id"]).first.to_json
   end
 
 end
