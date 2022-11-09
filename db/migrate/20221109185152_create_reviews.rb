8cb8b915cf5accedfe8badf962ee34cba77e3e9c
Titus Mumo <titusmumo@gmail.com>
2022-11-09 21:55:21 +0300

    create review migration file

--- db/migrate/20221109185152_create_reviews.rb
+++ db/migrate/20221109185152_create_reviews.rb
@@ -1,6 +1,6 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-    create_table reviews do |t|
+    create_table :reviews do |t|
       t.integer :customer_id
       t.integer :product_id
       t.string :comment
