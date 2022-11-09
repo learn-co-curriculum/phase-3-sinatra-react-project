08c3d181d7c66cddae7df8e3cb81d821a03f875c
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-11-09 03:51:12 +0300

    added some get methods for apartments and appointments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,46 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/apartments" do
+    all_apartments = Apartment.all 
+    all_apartments.to_json
+  end
+
+  get "/apartments-rent/:rent" do
+    apartment_by_rent = Apartment.find_by(rent: params[:rent])
+    apartment_by_rent.to_json
+  end
+
+  get "/apartments-name/:name" do
+    apartment_by_name = Apartment.find_by(title: params[:name]) #change this to lower case
+    apartment_by_name.to_json
+  end
+
+  get "/locations" do 
+    all_locations = Location.all 
+    all_locations.to_json
+  end
+
+  get "/apartment-by-location/:location" do
+    location = Location.find_by(name: params[:location])
+    location.apartments.to_json
+  end
+  
+  # get "apartments-by-rooms/:rooms" do
+  #   by_rooms = Apartment.find_by(room: params[:rooms])
+  #   by_rooms.to_json
+  # end
+
+  get "/appointments" do
+    all_appointments = Appointment.all 
+    all_appointments.to_json
+  end
+
+  post "/appointments" do
+    new_appointment = Appointment.create(
+      client_id: params[:client_id],
+      apartment_id: params[:apartment_id]
+    )
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,21 @@ puts "🌱 Seeding spices..."
 # location5=Location.create(name: "Juja")
 # location6=Location.create(name: "Langata")
 
-# apartments dummy
-apartment1=Apartment.create(title: "Gekonyo apartments", room: 2, image: "https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80.jpg", description: "great spacious", rent: 25000, caretaker_contact: +254728748442)
-apartment2=Apartment.create(title: "Victoria apartments", room: 1, image: "https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80.jpg", description: "nice kitchen area", rent: 30000, caretaker_contact: +254728748432)
-apartment3=Apartment.create(title: "Qwetu", room: 3, image: "https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80.jpg", description: "great balcony", rent: 18000, caretaker_contact: +254728748942)
+# # apartments dummy
+# apartment1=Apartment.create(title: "Gekonyo apartments", room: 2, image: "https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80.jpg", description: "great spacious", rent: 25000, caretaker_contact: +254728748442)
+# apartment2=Apartment.create(title: "Victoria apartments", room: 1, image: "https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80.jpg", description: "nice kitchen area", rent: 30000, caretaker_contact: +254728748432)
+# apartment3=Apartment.create(title: "Qwetu", room: 3, image: "https://images.unsplash.com/photo-1628592102751-ba83b0314276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80.jpg", description: "great balcony", rent: 18000, caretaker_contact: +254728748942)
 
-# client dummy data
-client1=Client.create(name: "Helga", age: 32, phone_number: +254784445232)
-client2=Client.create(name: "Sharon", age: 16, phone_number: +254759493922)
-client3=Client.create(name: "Zak", age: 24, phone_number: +254756977132)
-client4=Client.create(name: "Joash", age: 26, phone_number: +254758382788)
-client5=Client.create(name: "Stephen", age: 45, phone_number: +254793220222)
+# # client dummy data
+# client1=Client.create(name: "Helga", age: 32, phone_number: +254784445232)
+# client2=Client.create(name: "Sharon", age: 16, phone_number: +254759493922)
+# client3=Client.create(name: "Zak", age: 24, phone_number: +254756977132)
+# client4=Client.create(name: "Joash", age: 26, phone_number: +254758382788)
+# client5=Client.create(name: "Stephen", age: 45, phone_number: +254793220222)
 
-# appointment dummy 
-appointment1=Appointment.create(client_id: 1, apartment_id: 2)
-appointment2=Appointment.create(client_id: 3, apartment_id: 1)
-appointment3=Appointment.create(client_id: 3, apartment_id: 3)
+# # appointment dummy 
+# appointment1=Appointment.create(client_id: 1, apartment_id: 2)
+# appointment2=Appointment.create(client_id: 3, apartment_id: 1)
+# appointment3=Appointment.create(client_id: 3, apartment_id: 3)
 
 puts "✅ Done seeding!"
