53e0592500a66540a577e3aecbbb31f131de8fd4
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-11-09 11:06:39 +0300

    made changes to book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,11 @@
 class Book < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
-    belongs_to :category
 	belongs_to :genre
     
 	def self.search(search)
 		if search
 			genre = Genre.find_by(title: search)
-			category = Category.find_by(title: search)
 
 		else
 			Book.all
