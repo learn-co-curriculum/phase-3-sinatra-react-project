3dc0125e7f449ab5015dc0d292eeedcc9db326de
Lynn W Mwaniki <mwanikilynn8@gmail.com>
2022-11-10 10:40:14 +0300

    created CRUD actions for models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,93 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  ##CRUD agencies route
+  get "/agencies" do
+    Agency.all.to_json
+    #(include: :properties)
+  end
+
+  get "/agencies/:id" do
+    Agency.where(id: params["id"]).first.to_json
+  end
+
+  post "/agencies" do
+    agency = Agency.new(params)
+
+    if agency.save
+      agency.to_json
+    else 
+      halt 422
+    end
+  end
+
+  delete "/agencies/:id" do
+    agency = Agency.where(id: params["id"]).first
+
+    if agency.destroy
+      {succes: "ok"}.to_json
+    else 
+      halt 500
+    end
+  end
+
+###CRUD properties route
+  get "/properties" do
+    Property.all.to_json
+  end
+
+  get "/properties/:id" do
+    Property.where(id: params["id"]).first.to_json
+  end
+
+  post "/properties" do
+    property = Property.new(params)
+
+    if property.save
+      property.to_json
+    else 
+      halt 422
+    end
+  end
+
+  delete "/properties/:id" do
+    property = Property.where(id: params["id"]).first
+
+    if property.destroy
+      {succes: "ok"}.to_json
+    else 
+      halt 500
+    end
+  end
+
+##CRUD clients route
+  get "/clients" do
+    Client.all.to_json
+    #(include: :properties)
+  end
+
+  get "/clients/:id" do
+    Client.where(id: params["id"]).first.to_json
+  end
+
+  post "/clients" do
+    client = Client.new(params)
+
+    if client.save
+      client.to_json
+    else 
+      halt 422
+    end
+  end
+
+  delete "/clients/:id" do
+    client = Client.where(id: params["id"]).first
+
+    if client.destroy
+      {succes: "ok"}.to_json
+    else 
+      halt 500
+    end
+  end
+
 end
