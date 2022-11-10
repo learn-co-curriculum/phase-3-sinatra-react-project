6591a40bc7b1e851d2c183c83c603aab172bc40c
hamsa <hafieekhan@gmail.com>
2022-11-10 19:20:39 +0300

    "Updated"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 class Comment <ActiveRecord::Base
     has_many :posts
     has_many :users,through: :posts
-=======
-class Comment < ActiveRecord::Base
-  has_many :posts
-  has_many :users,through: :posts
->>>>>>> 235b971c5a9dad1f2779829ee36a78c7256abada
 end
