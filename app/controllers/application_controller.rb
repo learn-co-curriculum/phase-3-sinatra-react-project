f9e59244c5e56572befbbc75947ad5e4011ed8fb
RoyOkite <royokite@gmail.com>
2022-11-10 19:51:49 +0300

    added reviews routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Routes
   get "/" do
     { group_five: "Kickbox ecommerce project", members: ["Roy", "Titus", "Angela", "Ruth"] }.to_json
   end
-
+  
+  # Customer Routes
   get "/customers" do
     Customer.all.to_json
   end
@@ -14,6 +14,7 @@ class ApplicationController < Sinatra::Base
     Customer.where(id: params["id"]).first.to_json
   end
 
+  # Product Routes
   get "/products" do
     Product.all.to_json
   end
@@ -22,6 +23,7 @@ class ApplicationController < Sinatra::Base
     Product.where(id: params["id"]).first.to_json
   end
 
+  # Order Routes
   get "/orders" do
     Order.all.to_json
   end
@@ -30,12 +32,32 @@ class ApplicationController < Sinatra::Base
     Order.where(id: params["id"]).first.to_json
   end
 
+  # Order Detail Routes
   get "/order_details" do
     OrderDetail.all.to_json
   end
 
   get "/order_details/:id" do
     OrderDetail.where(id: params["id"]).first.to_json
+  end
+
+  # Review Routes
+  get "/reviews" do
+    Review.all.to_json
+  end
+
+  get "/reviews/:id" do
+    Review.where(id: params["id"]).first.to_json
+  end
+
+  post "/reviews" do
+    review = Review.new(params)
+
+    if review.save
+      review.to_json
+    else
+      halt 422
+    end
   end
 
 end
