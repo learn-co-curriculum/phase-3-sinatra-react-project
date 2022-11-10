47b4285c22fa6bf95a4829f7d4c0ecb07daf19c0
Timothy <lemayiant4@gmail.com>
2022-11-10 23:02:56 +0300

    add car post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < Sinatra::Base
     car = Car.find(params[:id])
     car.to_json
   end
+  post '/add_car' do 
+    car = Car.create(
+      category: params[:category],
+      model: params[:model],
+      fuel_type: params[:fuel_type],
+      price: params[:price],
+      seats: params[:seats],
+      year: params[:year],
+      front_url:params[:front_url],
+      back_url:params[:back_url],
+      side_url: params[:side_url],
+      inside_url:params[:inside_url],
+      transmission: params[:transmission]
+    )
+    car.to_json
+  end
 end
