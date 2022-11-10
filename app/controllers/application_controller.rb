cca44fef570f929227ad6c7513b6d0eb373c0364
Mulu <brianmulu007@gmail.com>
2022-11-10 10:11:51 +0300

    Addition of routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,44 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/books" do
-    { message: "Good luck with your project!" }.to_json
+    books = Book.all.order(:id)
+    books.to_json
+  end
+
+  get '/books/:id' do
+    book = Book.find(params[:id])
+    book.to_json
+  end
+
+  delete "/books/:id" do
+    deleted = Book.find(params[:id])
+    deleted.destroy
+    deleted.to_json
+  end 
+
+  post "/books" do
+    new_book = Book.create(
+      title: params[:title],
+      genre: params[:genre],
+      price: params[:price],
+      description: params[:description],
+      year_published: params[:year_published],
+      user_id: params[:user_id]
+    )
+    new_book.to_json
+  end
+
+    patch "/books/:id" do 
+    updated = Book.find(params[:id])
+    updated.update(
+      title: params[:title],
+      genre: params[:genre],
+      price: params[:price],
+      description: params[:description],
+      year_published: params[:year_published],
+      user_id: params[:user_id]
+    )
+    updated.to_json
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,4 @@
 class Review < ActiveRecord::Base
     belongs_to :user
-<<<<<<< HEAD
-
-    
-=======
     belongs_to :book
->>>>>>> main
 end
