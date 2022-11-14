4c52196c73ad2e2b4ac5585f9dd5c8ea11e1f3ae
Adam Khodier <khodieradam1297@gmail.com>
2022-11-14 17:16:02 -0500

    more changes

--- project-3-client/src/components/SearchFilter.js
+++ project-3-client/src/components/SearchFilter.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function SearchFilter() {
   return (
-    <div></div>
+    <div className='search'>
+      <h2>Destination Search</h2>
+      <input
+        className='search-input'
+        type='text'
+        placeholder='Search for Destinations!'
+      ></input>
+    </div>
   )
 }
 
