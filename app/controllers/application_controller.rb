109d631eec763d3e4870516fb809a4a97b6549b9
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-14 13:38:53 -0500

    Adding stuff two

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    #{ message: "Good luck with your project!" }.to_json
+    { message: "HW wuz here" }.to_json
   end
 
 end
