6807b4f0d714dfe4998b4037fbe16446d574f5b2
Dulat <dulat.kussainov@gmail.com>
2022-11-15 12:16:41 -0500

    latest updates on tables

--- db/migrate/20221114201351_create_posts.rb
+++ db/migrate/20221114201351_create_posts.rb
@@ -6,7 +6,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.boolean :is_liked
       t.string :image_url
       t.datetime :date_posted
-      t.string :user_posted_username
       
       t.timestamps
     end    
