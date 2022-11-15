42c1795b107ae61a3c63d25de7e55df7b34458b9
Adam Kabak <adamkabak@yahoo.com>
2022-11-15 16:38:04 -0500

    good 2 go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,23 +8,29 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
-
+  
   get "/users/:user_name" do
+    
+      User.find_by(user_name: params[:user_name]).to_json
+    
     # binding.pry
-    User.find_by(user_name: params[:user_name]).to_json
   end
-
+  
+  get "/users/:id" do
+    user = User.find(params[:id])
+    user.to_json
+  end
 
   get "/users" do
     User.all.to_json
   end
 
+  # get "/users/1" do
+  #   User.first.to_json
+  # end
+
   
 
-  get "/users/:id" do
-    user = User.find(params[:id])
-    user.to_json
-  end
 
   get "/stocks" do
     Stock.all.to_json(include:{stock_price:{only:[:price]}})
@@ -32,6 +38,7 @@ class ApplicationController < Sinatra::Base
 
   get "/users/:user_name/watchlist" do
     User.find_by(user_name: params[:user_name]).watchlist.stocks.to_json(include:{stock_price:{only:[:price]}})
+    # User.find_by(user_name: params[User.first.user_name]).watchlist.stocks.to_json(include:{stock_price:{only:[:price]}})
   end
 
 
