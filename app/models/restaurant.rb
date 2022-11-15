28331a22d3d1bb8ace3e8968a3cbf020a65ca0fb
Chris Li <chris.lty07@gmail.com>
2022-11-15 18:07:13 -0500

    Updated Database

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,17 @@ class Restaurant < ActiveRecord::Base
     has_many :reviews
     accepts_nested_attributes_for :restaurant_images
     has_many :users, through: :reviews
+
+    # def self.find_nearby_restaurants(zipcode)
+    #     nearyby_restaurants_array = []
+    #     Restaurant.all.map do |r|
+    #         lat1 = r.latitude
+    #         lon1 = r.longitude
+
+    #         binding.pry
+    #         clean_data
+
+            
+    #     end
+    # end
 end
