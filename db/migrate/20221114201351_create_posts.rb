df4ec9c11130fae952bf38fb8b8ab1867939f151
Alexis Torosina <alexistorosina1@gmail.com>
2022-11-16 11:32:53 -0500

    asd

--- db/migrate/20221114201351_create_posts.rb
+++ db/migrate/20221114201351_create_posts.rb
@@ -6,10 +6,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.boolean :is_liked
       t.string :image_url
       t.datetime :date_posted
-<<<<<<< HEAD
-=======
       t.string :comments
->>>>>>> dulat
       
       t.timestamps
     end    
