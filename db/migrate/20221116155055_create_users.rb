59274dbc02f8564149b8b031ee149e51671f4e9c
Jaspreet kaur <102193406+Jaspreet9795@users.noreply.github.com>
2022-11-16 13:02:00 -0800

    Update 20221116155055_create_users.rb

--- db/migrate/20221116155055_create_users.rb
+++ db/migrate/20221116155055_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.string :user_role
+      t.string :email
       t.string :password 
       t.string :profile_pic
       t.timestamps
