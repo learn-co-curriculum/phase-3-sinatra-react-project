19a6492e7303746ee358190c5981640e08e1f120
Adam Khodier <khodieradam1297@gmail.com>
2022-11-16 14:48:35 -0500

    add dest form

--- project-3-client/src/components/AddDestinationsForm.js
+++ project-3-client/src/components/AddDestinationsForm.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 function AddDestinationsForm() {
   return (
-    <div>
+    <div className='destination_form'>
+      <form className='ui_form'>
+        <input
+          className='city_name'
+          type="text"
+          name="city"
+          placeholder='Add City Name'
+        ></input>
+          <input
+            className="image_url"
+            type="text"
+            name="url"
+            placeholder="Add Image URL"
+          ></input>
+          <input
+            className="country_name"
+            type="text"
+            name="country"
+            placeholder="Add Country Name"
+          ></input>
+          <label for="cars">Pick a Continent:</label>
+          <select name='cars'>
+            <optgroup label='Continents'>
+            <option>North America</option>
+            <option>South America</option>
+            <option>Europe</option>
+            <option>Africa</option>
+            <option>Asia</option>
+            <option>Australia</option>
+            <option>Antarctica</option>
+            </optgroup>
+          </select>
+          <button>Add Destination</button>
+      </form>
       </div>
   )
 }
