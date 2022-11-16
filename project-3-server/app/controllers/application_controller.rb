d67f338b771286e5f6dc882ae5d719dcdc046d42
Mariia <m.goncharenkoo@icloud.com>
2022-11-16 09:19:13 -0500

    worked on controllers

--- project-3-server/app/controllers/application_controller.rb
+++ project-3-server/app/controllers/application_controller.rb
@@ -6,4 +6,61 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  
+  get "/continents" do
+    continents = Continent.all 
+    continents.to_json
+  end
+  
+  get '/continents/:id' do
+    continent = Continent.find(params[:id])
+    continent.to_json
+  end
+
+  get "/destinations" do
+    destinations = Destination.all 
+    destinations.to_json
+  end
+
+  get "/destinations/:id" do
+    destination = Destination.find(params[:id])
+    destination.to_json
+  end
+
+  post "/destinations" do
+    destination = Destination.create(
+      city_name: params[:city_name],
+      country_name: params[:country_name],
+      img_url: params[:img_url],
+      continent_id: params[:continent_id]
+    )
+    destination.to_json
+  end
+
+  delete "/destinations/:id" do
+    destination = Destination.find(params[:id])
+    destination.destroy
+    destination.to_json
+  end
+
+  get "/reviews" do
+    reviews = Review.all 
+    reviews.to_json
+  end
+
+  get "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json
+  end
+
+  get "/users" do
+    users = User.all
+    users.to_json
+  end
+
+  get "/users/:id" do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
 end
--- project-3-server/app/models/Destination.rb
+++ project-3-server/app/models/Destination.rb
@@ -2,4 +2,16 @@ class Destination < ActiveRecord::Base
     belongs_to :continent
     has_many :reviews
     has_many :users, through: :reviews
+
+    def number_of_reviews
+        self.reviews.count
+    end
+
+    # def self.by_amount_of_reviews
+    #     self.order(Destination.first.reviews.count)
+    # end
+
+    # def self.by_date_of_creation
+    #     self.order(:create_at)
+    # end
 end
