f9c8df8af47b81122e1d3838865aa81692c463da
Adam Khodier <khodieradam1297@gmail.com>
2022-11-16 10:39:44 -0500

    added NavBar.js

--- project-3-client/src/components/App.js
+++ project-3-client/src/components/App.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import DestinationsContainer from "./DestinationsContainer";
 import FavoriteDestinations from "./FavoriteDestinations";
 import AddDestinationsForm from "./AddDestinationsForm";
+import NavBar from './NavBar';
 
 function App() {
 
@@ -17,20 +18,7 @@ function App() {
 
   return (
     <>
-    <h1>Travel Destinations Application</h1>
-    <nav className='nav'>
-    <ul className='links'>
-    <li>
-      <Link to="/home_destinations">Home Destinations</Link>
-    </li>
-    <li>
-      <Link to="/add_new_destinations">Add New Destination</Link>
-    </li>
-    <li>
-      <Link to="/favorite_destinations">Favorite Destinations</Link>
-    </li>
-    </ul>
-    </nav>
+    <NavBar/>
     <Routes>
       <Route path="/home_destinations" element={<DestinationsContainer destinations={destinations}/>}/>
       <Route path="/add_new_destinations" element={<AddDestinationsForm/>}/>
---
+++ project-3-client/src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <div>
+    <h1>Travel Destinations Application</h1>
+    <nav className='nav'>
+    <ul className='links'>
+    <li>
+    <Link to="/home_destinations">Home Destinations</Link>
+    </li>
+    <li>
+    <Link to="/add_new_destinations">Add New Destination</Link>
+    </li>
+    <li>
+    <Link to="/favorite_destinations">Favorite Destinations</Link>
+    </li>
+    </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default NavBar
