b437b1512fef809eba5d5899cc10be433acedcdd
TaylanPostalci <tay.postal@gmail.com>
2022-11-17 14:49:54 -0500

    added back routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 require "pry"
-
-
 class UsersController < ApplicationController 
-
     post "/signup" do
       user = User.new(username: params[:username], password: params[:password], email: params[:email], name: params[:name])
       if user.username.blank? || user.email.blank? || user.name.blank? || user.password.blank? || User.find_by_email(params[:email]) || User.find_by_username(params[:username])
@@ -13,7 +10,21 @@ class UsersController < ApplicationController
         return {user: user}.to_json
       end
     end
-
+    get "/profile" do
+      # p session[:user_id]
+      user = User.find(session[:user_id])
+      if user
+        return {user: user}.to_json(
+          include: {
+            posts: {
+              include: [comments: {include: :commented_user}]
+              }
+          }
+        ) # here
+      else
+          return {error: "User not found"}.to_json
+      end
+    end
     post "/login" do 
       user = User.find_by_username(params[:username])
       if user && user.authenticate(params[:password])
@@ -38,13 +49,8 @@ class UsersController < ApplicationController
 
     get "/logout" do 
       session.clear
-      return ("Logged out successfully!").to_json
-    end
-
-      get "/users" do
-        users = User.all.to_json
-      end
-
+      return ("Please, login...").to_json
+@@ -58,6 +64,9 @@ class UsersController < ApplicationController
       get "/users/:id" do
         user = User.find(params[:id]).to_json
       end
@@ -55,13 +61,11 @@ class UsersController < ApplicationController
       get "/users/username/:username" do
         user = User.find_by(username: params[:username]).to_json
       end
-
       patch "/users/:id" do
         user = User.find(params[:id])
         user.update(params )
         user.to_json
       end
-
       delete "/users/:id" do
         user = User.find(params[:id])
         user.destroy
