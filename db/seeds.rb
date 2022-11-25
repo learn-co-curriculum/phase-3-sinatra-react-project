765c7ec2a12b2014103d8657c163b1d1694352d3
Win Feigle <win.feigle@gmail.com>
2022-11-24 17:09:11 -0700

    Add restaurants to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Restaurant.create[
+    name: "Chipotle Mexican Grill", 
+    address: "1600 California St Unit 7, Denver, CO 80202", 
+    logo_url: "https://www.chipotle.com/content/dam/poc/order/images/logos/cmg-medallion-logo.svg"
+    ]
+
+Restaurant.create[
+    name: "Modern Market Eatery", 
+    address: "900 16th St Mall, Denver, CO 80202",
+    logo_url: "https://modernmarket.com/static/media/logo.493c6ed99fcbd4b8edb493211e512775.svg"
+    ]
+
+Restaurant.create[
+    name: "Noodles & Company", 
+    address: "303 16th St Mall Suite 150, Denver, CO 80202",
+    logo_url: "https://1000logos.net/wp-content/uploads/2022/10/Noodles-and-Company-logo.png"
+    ]
 
 puts "✅ Done seeding!"
