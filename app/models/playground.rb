867c4e70ffce8d205aee08ae9b7d45b582c0abe7
Victoria Parry <vlparry@gmail.com>
2022-11-28 11:20:18 -0500

    figuring out playground validation

--- app/models/playground.rb
+++ app/models/playground.rb
@@ -1,7 +1,7 @@
 class Playground < ActiveRecord::Base
     belongs_to :town
 
-    # validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true
  end
   
 
