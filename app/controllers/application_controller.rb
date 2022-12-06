bfb76d1d55c3d6ac263c85d2101dfff279cfc87f
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-12-06 14:27:01 -0500

    update main branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/albums' do
+    album = Album.all
+    album.to_json
+  end
+
+  get '/users' do
+    user = User.all
+    user.to_json
+  end
+
+  get'/reviews' do
+    review = Review.all
+    review.to_json
   end
 
 end
