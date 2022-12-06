38355a5b7e2fcba8d3a2772b6fde18cde8599162
Yuriko <ygabriela.arnott@gmail.com>
2022-12-06 16:06:29 -0500

    gaby changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,3 +24,9 @@ end
 #   bubbleteas = Bubbletea.find(params[:id])
 #   bubbleteas.to_json
 # end
+
+#1 get render all the info in bubbleteas
+#2 Post Order form : inputs => Base,Topping1,Topping2,size(select) Button submit (fetch Bubbletea - Fetch Order)
+#3 Update Order form button - Pop up Delete button
+#4Navbar should show in all the pages
+
