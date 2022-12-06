3c425282dd3097c66f58ad4f16f77c0cdc509f93
Abena Adu <abeanadu@gmail.com>
2022-12-06 14:04:40 -0500

    Created backend repo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,20 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #SHOW
+  #Example of route
+  get "/sweaters/:id"
+  Sweater.find(params[:id]).to_json
+  end
+
+  #Example of Post Malone
+  post "/post" do
+    binding.pry
+  end
+
+
+  #UPDATE
+  patch "/sweaters/:id" do
+    
+  end
 end
