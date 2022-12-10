c132440e7371c2426ff0d838adabe2713e6cb43f
Asma <as1588ma@gmail.com>
2022-12-10 11:37:31 -0500

    create get end point by id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,10 @@ class BooksController < ApplicationController
     get '/books' do
         Book.all.to_json
     end
+
+    get '/books/:id' do
+        book = Book.find(params[:id])
+        book.to_json
+    end
 end
+
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,9 @@ class ReviewsController < ApplicationController
     get '/reviews' do
         Review.all.to_json
     end
+
+    get '/reviews/:id' do
+        review = Review.find(params[:id])
+        review.to_json
+    end
 end
