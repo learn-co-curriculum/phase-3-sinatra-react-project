b52e4acee3e7c5f15f1d01985b95bef297e648f4
frank <franksavena@gmail.com>
2023-01-03 08:26:57 -0600

    Sinatra routes and update delilverables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,45 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # At a minimum, set up the following API routes in Sinatra:
+  # create and read actions for both models
+  # full CRUD capability for one of the models
+
+
+  #Full CRUD Post Model 
+  get "/posts" do
+    posts = Post.all.order(:created_at, :ASC)
+    posts.to_json
   end
+
+  post './posts' do
+    post = Post.create(
+      title: params[:title],
+      body: params[:body],
+      user_id: params[:user_id],
+    )
+    post.to_json
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+    post.update(
+      title: params[:title],
+      body: params[:body],
+    )
+    post.to_json
+  end
+
+  delete '/posts/:id' do
+    post = Post.find(params[:id])
+    post.destroy
+    post.to_json
+  end
+
+  #CRUD for comments 
+
+
+  #Create and read for users 
+
 
 end
