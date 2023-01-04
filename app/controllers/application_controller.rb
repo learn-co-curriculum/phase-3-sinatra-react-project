1c7afde66ca41b822aae7d430c5aa11d92c4dbb9
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-04 13:18:15 -0500

    backend crud for tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get "/tasks" do
+    tasks = Task.all
+    tasks.to_json
+  end
+
+  get "/tasks/:id" do
+    task = Task.find(params[:id])
+    task.to_json
+  end
+
+  post '/tasks' do
+    task_to_add = Task.create(
+      description: params[:description],
+      category_id: params[:category_id],
+      day_id: params[:day_id]
+    )
+    task_to_add.to_json
+  end
+
+  patch '/tasks/:id' do
+    task_to_update = Task.find(params[:id])
+    task_to_add.update(description: params[:description], category_id: params[:category_id, :day_id])
+  end
+
+  delete 'tasks/:id' do
+    task_to_delete = Task.find(params[:id])
+    task_to_delete.delete
   end
 
 end
