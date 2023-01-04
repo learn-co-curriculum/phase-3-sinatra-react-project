169575e8242d216d476b26054cdd7aea0d3121f8
Kevin Diesenberg <kdiesenb@gmail.com>
2023-01-04 13:27:40 -0500

    backend crud for category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,12 +22,40 @@ class ApplicationController < Sinatra::Base
 
   patch '/tasks/:id' do
     task_to_update = Task.find(params[:id])
-    task_to_add.update(description: params[:description], category_id: params[:category_id, :day_id])
+    task_to_update.update(description: params[:description], category_id: params[:category_id, :day_id])
   end
 
   delete 'tasks/:id' do
     task_to_delete = Task.find(params[:id])
     task_to_delete.delete
+  end
+
+  get '/categories' do
+    category = Category.all
+    category.to_json
+  end
+
+  get '/categories/:id' do
+    category = Category.find(params[:id])
+    category.to_json
+  end
+
+  post '/categories' do
+    category_to_add = Category.create(
+      urgent: params[:urgent],
+      day_id: params[:day_id],
+      task_id: params[:task_id]
+    )
+    category_to_add.to_json
+  end
+
+  patch '/categories/:id' do
+    category_to_update.update(urgent: params[:urgent], day_id: params[:day_id, task_id: params[:task_id])
+  end
+
+  delete '/categories/:id' do
+    category_to_delete = Category.find(params[:id])
+    category_to_delete.delete
   end
 
 end
