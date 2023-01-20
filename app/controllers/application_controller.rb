c4004a8fc1638268b3bce39fe0f84296637fed0c
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2023-01-20 17:47:41 -0500

    Added additional sinatra routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,48 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
   get "/users" do
     users = User.all
+    users.to_json
+  end
+
+  get "/users/:id" do
+    user = User.find(params[:id])
     user.to_json(include: :tickets)
   end
 
   get "/concerts" do
     concerts = Concert.all
+    concerts.to_json
+  end
+
+  get "/concerts/:id" do
+    concert = Concert.find(params[:id])
     concert.to_json
   end
 
-  post '/users' do
-    newUser = User.create(name:params[:name], password:params[:password], email: params[:email])
-    newUser.to_json
+  get "/tickets" do
+    tickets = Ticket.all
+    tickets.to_json
   end
 
-  post '/concerts' do
-    newConcert = Concert.create(artist:params[:artist], date:params[:date], num_tickets: params[:num_tickets])
-    newConcert.to_json
+  get "/tickets/:id" do
+    ticket = Ticket.find(params[:id])
+    ticket.to_json(include: :concert)
+  end
+
+  post "/tickets" do
+    newTicket = Ticket.create(concert_id: params[:concert_id], user_id: params[:user_id])
+    newTicket.to_json
+  end
+
+  patch "/users/:id" do
+    user = User.find(params[:id])
+    user.update(name: params[name], password: params[:password], email: params[:email], tickets: params[:tickets])
+    user.to_json
+  end
+
+  delete "/tickets/:id" do
+    ticket = Ticket.find(params[:id])
+    ticket.destroy
   end
 
 end
