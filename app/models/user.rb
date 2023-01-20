8541ccbbab3097bbfc1565cb88ed3dfa8eeccf6b
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2023-01-20 11:14:44 -0500

    Added refund function

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,23 @@ class User < ActiveRecord::Base
     end
 
     def refund_tickets(concert, num_tickets)
+        tickets_to_concert = tickets.where(concert_id: concert.id)
+        if tickets_to_concert.length < 1
+            puts "#{self.name} owns no tickets to this concert."
+            return 0
+        end
+
+        if tickets_to_concert.length < num_tickets
+            puts "Refunding #{tickets_to_concert.length} tickets owned insead of requested #{num_tickets}."
+            num_tickets = tickets_to_concert.length
+        end
+
+        for i in 0..num_tickets-1 do
+            Ticket.destroy(tickets_to_concert[i].id)
+        end
+
+        Concert.update(concert.id, :unsold_tickets => concert.unsold_tickets+num_tickets)
+        puts "Your #{num_tickets} tickets to #{concert.artist} on #{concert.date} have been successfully refunded."
         return 1
     end
 end
