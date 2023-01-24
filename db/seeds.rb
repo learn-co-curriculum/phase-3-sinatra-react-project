4089396e350b83d1396811aeb234488412e5a14a
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-01-24 12:17:57 -0500

    first artist seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding artists and paintings..."
 
 # Seed your database here
+
+a1 = Artist.create(first_name: "Frida", last_name: "Kahlo", img_link: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vogue.fr%2Ffashion-culture%2Farticle%2Ffrida-kahlo-7-things-to-know-about-the-artist&psig=AOvVaw3RgOkknnEA6G2nGoIshsWo&ust=1674590274439000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKCvx4W93vwCFQAAAAAdAAAAABAK" )
+
+
 
 puts "✅ Done seeding!"
