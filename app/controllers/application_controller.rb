8baf3321335bac967549010cb6095775f69d7513
JamesPrawdzik <sebaprawdzik@gmail.com>
2023-01-25 13:53:01 -0500

    app controller changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,53 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-
   # fetch for all bars on "home page"
   get '/bars' do
     bars = Bar.all
-    bars.to_json
+    bars.to_json(only: [:name, :category, :price], include: [:reviews])
   end
- # (include: {reviews: {only: [:star_rating, :content]}}) @ end of line 7
 
 #  get for each individual bar in bar info
   get '/bars/:id' do
     bar = Bar.find(params[:id])
-    bar.to_json(include: {reviews: {only: [:star_rating, :content]}})
+    bar.to_json(only: [:name, :location ,:category, :price, :closing_time], include: {reviews: {only: [:star_rating, :content]}})
   end
 
-  # post '/bars' do
-  #   bar = Bar.create(name:params[:name], category:params[:category], price:params[:price], closing_time:params[:closing_time], location:params[:location])
-  #   bar.to_json
-  # end
-
-  # delete '/bars/:id' do
-  #   bar = Bar.find(params[:id])
-  #   bar.destroy
-  #   bar.to_json
-  # end
-
-  # edit so that it gets all reviews of a specific user only
   get '/reviews' do
     reviews = Review.all
     reviews.to_json
   end
-
-
-  # here just in case indiviuall review is needed
+  
   get '/reviews/:id' do
     review = Review.find(params[:id])
-    review.to_json
+    review.to_json(only: [:star_rating, :content])
   end
-
-
-  # post so client can add their own review
+  
   post '/reviews' do
     review = Review.create(star_rating:params[:star_rating], content:params[:content], bar_id:params[:bar_id], user_id:params[:user_id])
     review.to_json
   end
-
-
-  # delete so client can delete their own review
+  
   delete '/reviews/:id' do
     review = Review.find(params[:id])
     review.destroy
@@ -55,3 +35,20 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# post '/bars' do
+#   bar = Bar.create(name:params[:name], category:params[:category], price:params[:price], closing_time:params[:closing_time], location:params[:location])
+#   bar.to_json
+# end
+
+# delete '/bars/:id' do
+#   bar = Bar.find(params[:id])
+#   bar.destroy
+#   bar.to_json
+# end
+
+
