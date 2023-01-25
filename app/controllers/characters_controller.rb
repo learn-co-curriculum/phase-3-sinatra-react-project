9a93fdc9ece23441ee234681c516f70bd19a56d2
Ryan Horn <rj.horn@gmail.com>
2023-01-25 12:35:40 -0700

    remove redirects, erb's in char_controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -7,12 +7,12 @@ class CharactersController < ApplicationController
 
   # GET: /characters/new
   get "/characters/new" do
-    erb :"/characters/new.html"
+    
   end
 
   # POST: /characters
   post "/characters" do
-    redirect "/characters"
+    
   end
 
   # GET: /characters/5
@@ -27,11 +27,11 @@ class CharactersController < ApplicationController
 
   # PATCH: /characters/5
   patch "/characters/:id" do
-    redirect "/characters/:id"
+    
   end
 
   # DELETE: /characters/5/delete
   delete "/characters/:id/delete" do
-    redirect "/characters"
+    
   end
 end
