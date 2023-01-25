4387810ddc4b1e138446039423c1a64716ace4d5
Ryan Horn <rj.horn@gmail.com>
2023-01-25 15:10:11 -0700

    new locations schema for img_url

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2023_01_24_193303) do
     t.string "name"
     t.string "terrain"
     t.string "inhabitants"
+    t.text "img_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
