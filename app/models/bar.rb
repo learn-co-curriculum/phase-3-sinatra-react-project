48e802d8c83de4801e1878e73176b78b37935003
tomer <tomertal52@yahoo.com>
2023-01-26 15:07:25 -0500

    minor changes

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -5,6 +5,7 @@ class Bar < ActiveRecord::Base
     # has_many :users, through: :favorite_bars
     has_many :bar_crawl_bars
 
+
     def average_rating
         self.reviews.average(:star_rating).to_f.round(1)
     end
