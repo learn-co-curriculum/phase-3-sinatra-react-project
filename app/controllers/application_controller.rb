9d9f631b7e9b4595afc136a2f929249a93bac471
Avinash Ramsumair <avi@pure235games.com>
2023-01-26 14:28:29 -0500

    bar_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
   # fetch for all bars on "home page"
   get '/bars' do
     bars = Bar.all
-    bars.to_json(only: [:name, :category, :price], include: [:reviews])
+    bars.to_json(only: [:id, :name, :category, :price])
   end
 
 #  get for each individual bar in bar info
--- db/migrate/05_create_bar_crawls.rb
+++ db/migrate/05_create_bar_crawls.rb
@@ -1,9 +1,9 @@
 class CreateBarCrawls < ActiveRecord::Migration[6.1]
   def change
     create_table :bar_crawls do |t|
+      t.string :bar_crawl_bars
       t.string :bar_crawl_name
       t.integer :user_id
-      t.integer :bar_id
     end
   end
 end
