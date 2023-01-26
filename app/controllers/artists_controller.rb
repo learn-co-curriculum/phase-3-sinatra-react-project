7112ba5918222487eb9fdb7c20779fa6db7c9b87
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-01-26 11:59:08 -0500

    controllers updated and separated and config

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,14 +12,14 @@ class ArtistsController < ApplicationController
 #   end
 
   #aka owners
-  get "/artists" do
+  get '/artists' do
     artists = Artist.all
     artists.to_json(include: :paintings)
   end
 
   #artists show route (owners) this is a dynamic route
   # getting the request, parsing it, in the params hash and key of id and value is 4 or whatever the id is
-  get "/artists/:id" do
+  get '/artists/:id' do
   #binding.pry
     artist = Artist.find_by(id: params[:id])
     if artist 
--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -3,25 +3,25 @@ class PaintingsController < ApplicationController
   #Paintings is Dogs  
 
 #aka dogs
- get "/paintings" do
+ get '/paintings' do
     paintings = Painting.all
     paintings.to_json
   end
 
-  #aka dogs this is the add
-#   post "/paintings" do
+  #aka dogs this is the add from the Painting Form 
+#   post '/paintings' do
 #     painting = Painting.create(params)
 #     painting.to_json
 #   end
 
   #aka dogs, delete does not return anything method delete
-#   delete "/paintings/:id" do 
+#   delete '/paintings/:id' do 
 #     painting = Painting.find_by(id: params[:id])
 #     painting.destroy
 #   end
 
   #update the painting' info
-#   patch "/paintings/:id" do
+#   patch '/paintings/:id' do
 #     painting = Painting.find_by(id: params[:id])
 #     painting.update(name: params[:name])
 #     painting.to_json
