16c17d3696aecde7a3c50ea23bd84ff13bc9e0fe
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-28 09:58:56 +0300

    Edit React client.

--- project-server/app/controllers/application_controller.rb
+++ project-server/app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # route to list all items
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    # { message: "Good luck with your project!" }.to_json
+    items = Item.all
+    items.to_json
   end
 
 end
