e8ec3772945bd0df5e8f3195bef7c1cfd9924e38
iamchacha021 <iamchacha021@gmail.com>
2023-03-01 17:57:01 +0300

    1. Create service data table 2. Create Api routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,40 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    services = Service.all
+    services.to_json
+  end
+
+  get '/services/:id' do
+    service = Service.find(params[:id])
+    service.to_json
+  end
+
+  post '/services' do
+    service = Service.create(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image_url: params[:image_url]
+    )
+
+    service.to_json
+  end
+
+  patch '/services/:id' do
+    service = Service.find(params[:id])
+    service.update(
+      description: params[:description],
+      price: params[:price]
+    )
+
+    service.to_json
+  end
+  
+  delete '/services/:id' do
+    service = Service.find(params[:id])
+    service.destroy
+    service.to_json
   end
 
 end
