a9f6271eba8c47a095557b9ab3481188de48a35e
chris-wamae <chriswamae123@gmail.com>
2023-03-02 20:33:57 +0300

    Created seeds to populate database

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+
+
+
+
+puts "🌱 Seeding pets and users..."
+
+# Seed your database here
+# User.create(username:"person1",password:"person1")
+# User.create(username:"person2",password:"person2")
+# User.create(username:"person3",password:"person3")
+# User.create(username:"person4",password:"person4")
+# User.create(username:"person5",password:"person5")
+
+# Pet.create(name:"pet1",breed:"breed1",image_url:"image1.com",user_id:1)
+# Pet.create(name:"pet2",breed:"breed2",image_url:"image2.com",user_id:1)
+# Pet.create(name:"pet3",breed:"breed3",image_url:"image3.com",user_id:1)
+# Pet.create(name:"pet4",breed:"breed4",image_url:"image4.com",user_id:1)
+# Pet.create(name:"pet5",breed:"breed5",image_url:"image5.com",user_id:2)
+# Pet.create(name:"pet6",breed:"breed6",image_url:"image6.com",user_id:2)
+# Pet.create(name:"pet7",breed:"breed7",image_url:"image7.com",user_id:3)
+# Pet.create(name:"pet8",breed:"breed8",image_url:"image8.com",user_id:4)
+# Pet.create(name:"pet9",breed:"breed9",image_url:"image9.com",user_id:5)
+
+puts "✅ Done seeding!"
+
