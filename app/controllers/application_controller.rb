523ca6d451a783b50f600cd0f9ed1d2b0860dc3c
chris-wamae <chriswamae123@gmail.com>
2023-03-03 08:54:42 +0300

    Added controller that returns all the pets for a specific user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < Sinatra::Base
     all_users = User.all
     all_users.to_json
   end
- 
+  
+   #returns all the pets for a specific user
+   get "/pets/:username" do
+    single_user = User.find_by(username:params[:username])
+    single_user.pets.to_json 
+  end
 
 end
