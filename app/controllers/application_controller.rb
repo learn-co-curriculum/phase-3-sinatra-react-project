b22c72b8894ec382aa2e06fce641971b6f9bbb6b
levy <lnjiru43@gmail.com>
2023-03-04 10:19:28 +0300

    controller for all the modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  configure do
+    enable :sessions
+    # set :session_secret, "secret"
+  end
   
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+
+
 
 end
