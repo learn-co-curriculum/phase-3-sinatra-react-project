eeb07cf432fd50cb4531115776246f7ef1dda5b3
levy <lnjiru43@gmail.com>
2023-03-04 14:40:39 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,33 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 # Create some users
-User.create(username: "levy", email: "levy@example.com", password: "22222")
-User.create(username: "njiru", email: "njiru@example.com", password: "22222")
+user1=User.create(username: "levy", email: "levy@example.com", password: "22222")
+user1=User.create(username: "njiru", email: "njiru@example.com", password: "22222")
+user1=User.create(username: "muguro", email: "muguro@example.com", password: "22222")
+user1=User.create(username: "nji", email: "nji@example.com", password: "22222")
+user1=User.create(username: "donlevy", email: "donlevy@example.com", password: "22222")
+user1=User.create(username: "donnjiru", email: "donnjiru@example.com", password: "22222")
 
+
+project1 = Project.create(
+    title: "project one",
+    description: "first"
+)
+
+project2 = Project.create(
+    title: "project two",
+    description: "two"
+)
+
+project3 = Project.create(
+    title: "project three",
+    description: "three"
+)
+
+project4 = Project.create(
+    title: "project four",
+    description: "four"
+)
 
 
 
