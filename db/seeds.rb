5ffccba6af8332209bffe130e69435dc1232b99d
levy <lnjiru43@gmail.com>
2023-03-04 14:08:35 +0300

    changed migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+# Create some users
+User.create(username: "levy", email: "levy@example.com", password: "22222")
+User.create(username: "njiru", email: "njiru@example.com", password: "22222")
+
+
+
 
 puts "✅ Done seeding!"
