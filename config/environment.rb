1bf7bd4bb7001c913837fa962d32621ba9d12844
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-06-06 11:32:32 +0300

    Changed sql

--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,25 @@ Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
 require_all 'app'
+
+
+
+# configure :development do
+#     ActiveRecord::Base.establish_connection(
+#     :adapter => "sqlite3",
+#     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+#   )
+#   end
+  
+#   configure :production do
+#     db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost/mydb')
+  
+#     ActiveRecord::Base.establish_connection(
+#       :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#       :host     => db.host,
+#       :username => db.user,
+#       :password => db.password,
+#       :database => db.path[1..-1],
+#       :encoding => 'utf8'
+#     )
+#   end
