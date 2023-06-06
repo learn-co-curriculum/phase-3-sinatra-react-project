9012fba6397c4d33a94f23e1bf5eb69727c865bf
gorety <wagutugorety@gmail.com>
2023-06-06 14:14:51 +0300

    controller

--- Rakefile
+++ Rakefile
@@ -5,7 +5,6 @@ desc "Start the server"
 task :server do  
   if ActiveRecord::Base.connection.migration_context.needs_migration?
     puts "Migrations are pending. Make sure to run `rake db:migrate` first."
-    return
   end
 
   # rackup -p PORT will run on the port specified (9292 by default)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,30 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  require './models/user'
+  require './models/message'
   
-  # Add your routes here
-  get "/messages" do
-    message = messages.all
-     messages.to_json
+  # 
+  get "/api/users" do
+    user = User.new(name: params[:name])
+     if user.save
+      #return a JSON response, redirect, or render a template
+     else
+      # handle validation errors or other failure cases
+     end
   end
 
-  get "/users" do
-    user = users.all
+# retrieve messages for a specific user
+  get "/api/users/:user_id/messages" do
+    user = users.find(params[:user_id])
     users.to_json
+  end
+
+  # update a message
+  put "/api/messages/:message_id" do
+    message = Message.find(params[:message_id])
+
+     if message.updated(content: params[:content])
 
 end
+
+
