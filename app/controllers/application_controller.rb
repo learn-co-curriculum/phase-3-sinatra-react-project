15331c9a5cb51dcb88023d303f3abde4b19e88e9
gorety <wagutugorety@gmail.com>
2023-06-06 13:48:38 +0300

    changes to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/messages" do
+    message = messages.all
+     messages.to_json
   end
+
+  get "/users" do
+    user = users.all
+    users.to_json
 
 end
