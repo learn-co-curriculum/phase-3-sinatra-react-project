662ff8aaa3ba4852125874318baff33058f5257a
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-06-06 11:12:20 +0300

    users table

---
+++ db/migrate/20230606075542_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :user_id
+      t.string :first_name
+      t.string :last_name
+      t.string :avatar
+    end
+  end
+end
