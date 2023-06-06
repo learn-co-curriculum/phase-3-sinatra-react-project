05af87cac3c00320ea88d2c59fee78b3d03830d0
joseph <josephmacharia575@gmail.com>
2023-06-06 12:48:05 +0300

    generate

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,14 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 
 puts "✅ Done seeding!"
+
+10.times do 
+    User.create(
+    user_id: Faker::User.user_id,
+    first_name: Faker::User.first_name,
+    last_name: Faker::User.last_name,
+    avatar: Faker::User.avatar,
+)
+end
+
+puts "Done seeding!"
