0c789b5c49b64a337f63b5542b70581c8b382778
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-06-07 22:03:39 +0300

    requests methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,26 @@ class ApplicationController < Sinatra::Base
     messages.to_json
   end
 
+  get '/users' do
+    users = User.all
+    users.to_json
+  end
+
   post '/messages' do
     user = User.find_by(id: params[:user_id])
     message = user.messages.create(text: params[:text])
     message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find_by(id: params[:id])
+    if message
+      message.destroy
+      "Message deleted successfully"
+    else
+      status 404
+      "Message not found"
+    end
   end
 
 end 
