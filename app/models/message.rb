8b2aa2d95d4948dfbe8c8b3e7e04f7fbd2e0760d
gorety <wagutugorety@gmail.com>
2023-06-07 11:46:45 +0300

    made changes to models

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ActiveRecord::Base
     belongs_to :user
-    # validates :content, presence :true 
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
     has_many :messages
-    # validates :name, presence :true
     
 end
