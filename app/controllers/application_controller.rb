6f3b9f6a6d1351cbdeadba251224b649f936af3d
gorety <wagutugorety@gmail.com>
2023-06-07 12:03:32 +0300

    controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,23 @@ require './models/user'
 require './models/message'
 
 class ApplicationController < Sinatra::Base
+  before do
+    # Implement user authentication logic here (e.g., checking authentication token)
+  end
 
- # controllers/messages_controller.rb
- 
+  # controllers/messages_controller.rb
+
   get '/messages' do
     messages = Message.all
     messages.to_json
   end
+
   post '/messages' do
-    message = Message.create(text: params[:text])
+    user = User.find_by(id: params[:user_id])
+    message = user.messages.create(text: params[:text])
     message.to_json
   end
-end
+ 
 end
 
 
